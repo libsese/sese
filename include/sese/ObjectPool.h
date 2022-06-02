7d5cdd514c9f8f58d87b38008e6bbd6d1fe944b7
kaoru <SHIINA_KAORU@OUTLOOK.COM>
2022-06-03 01:05:40 +0800

    对象池优化

--- include/sese/ObjectPool.h
+++ include/sese/ObjectPool.h
@@ -1,9 +1,15 @@
+/**
+ * @file ObjectPool.h
+ * @author kaoru
+ * @date 2022年6月2日
+ * @brief 对象池
+ */
 #pragma once
 #include <sese/Config.h>
 #include <sese/thread/Locker.h>
 #include <atomic>
 #include <functional>
-#include <list>
+#include <queue>
 
 #ifdef _WIN32
 #pragma warning(disable : 4251)
@@ -11,46 +17,89 @@
 
 namespace sese {
 
-    class API Recycler;
-    class API Recyclable {
+    /**
+     * 可回收对象
+     * @tparam T 数据类型
+     */
+    template<typename T>
+    class Recyclable {
     public:
-        friend class Recycler;
-        explicit Recyclable() noexcept = default;
-        virtual void recycle() noexcept;
-        [[nodiscard]] bool isRecycled() const noexcept { return _isRecycled; }
+        using Ptr = std::unique_ptr<Recyclable>;
+
+        virtual ~Recyclable() noexcept = default;
+
+        T getValue() const noexcept { return value; }
+        void setValue(T v) noexcept { this->value = v; }
 
     private:
-        Recycler *recycler = nullptr;
-        bool _isRecycled = true;
+        T value{};
     };
 
-    class API Recycler {
+    /**
+     * 回收器
+     * @tparam T 数据类型
+     */
+    template<typename T>
+    class Recycler {
     public:
-        using FuncNewObject = std::function<Recyclable *()>;
-        using ObjectStack = std::list<Recyclable *>;
         using SizeType = size_t;
         using MutexType = std::mutex;
+        using RecyclableType = Recyclable<T>;
+        using ObjectPool = std::queue<typename Recyclable<T>::Ptr>;
 
-        explicit Recycler(size_t baseSize, const Recycler::FuncNewObject &funcNewObject) noexcept;
-        ~Recycler() noexcept;
-
-        Recyclable *get() noexcept;
-
-        template<typename T>
-        inline T *getAs() {
-            return dynamic_cast<T *>(get());
+        /**
+         * 初始化一个对象池
+         * @param baseSize 基础大小
+         */
+        explicit Recycler(SizeType baseSize) noexcept {
+            size = baseSize;
+            available = baseSize;
+            for (SizeType i = 0; i < baseSize; i++) {
+                pool.push(std::make_unique<RecyclableType>());
+            }
         }
 
-        // todo 彻底释放部分资源
-        void recycle() noexcept;
+        /**
+         * 从对象池中获取一个可回收对象
+         * @return 可回收对象
+         */
+        typename Recyclable<T>::Ptr get() noexcept {
+            mutex.lock();
+            if (available == 0) {
+                size++;
+                mutex.unlock();
+                return std::make_unique<RecyclableType>();
+            } else {
+                available--;
+                auto front = std::move(pool.front());
+                pool.pop();
+                mutex.unlock();
+                return front;
+            }
+        }
 
-        void recycle(Recyclable *recyclable) noexcept;
+        /**
+         * @todo 彻底释放部分资源
+         * @deprecated 暂未实现
+         */
+        void recycle() noexcept {
+        }
 
-    protected:
+        /**
+         * 回收资源
+         * @param recyclable 可回收对象
+         * @warning 回收后将会置空原先指针(std::move)
+         */
+        void recycle(typename RecyclableType::Ptr &recyclable) noexcept {
+            Locker locker(mutex);
+            available++;
+            pool.push(std::move(recyclable));
+        }
+
+    private:
         MutexType mutex;
-        FuncNewObject newObject;
         SizeType size = 0;
         SizeType available = 0;
-        ObjectStack stack;
+        ObjectPool pool;
     };
 }// namespace sese
