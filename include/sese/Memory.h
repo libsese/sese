dd77f55e987e6dfb0dc65029528b63f299e8a9df
kaoru <SHIINA_KAORU@OUTLOOK.COM>
2022-06-03 13:52:50 +0800

    makeUnique、makeShared 别名

--- include/sese/Memory.h
+++ include/sese/Memory.h
@@ -13,6 +13,30 @@ namespace sese {
     using WeakType = std::weak_ptr<T>;
 
     /**
+     * 实例化 SharedType
+     * @tparam ReturnType SharedType 模板参数
+     * @tparam Args 模板参数的构造函数参数类型
+     * @param args 模板参数的构造函数参数
+     * @return SharedType<ReturnType>
+     */
+    template<typename ReturnType, typename... Args>
+    inline SharedType<ReturnType> makeShared(Args &&...args) {
+        return std::make_shared<ReturnType>(args...);
+    }
+
+    /**
+     * 实例化 UniqueType
+     * @tparam ReturnType UniqueType 模板参数
+     * @tparam Args 模板参数的构造函数参数类型
+     * @param args 模板参数的构造函数参数
+     * @return UniqueType<ReturnType>
+     */
+    template<typename ReturnType, typename... Args>
+    inline SharedType<ReturnType> makeUnique(Args &&...args) {
+        return std::make_unique<ReturnType>(args...);
+    }
+
+    /**
      * std::make_shared 搭配 std::initializer_list 使用
      * @tparam ReturnType 预计返回 std::shared_ptr 的模板类型
      * @tparam InitType std::initializer_list 模板参数
@@ -58,7 +82,7 @@ namespace sese {
     template<typename ReturnType, typename RawType>
     UniqueType<ReturnType> dynamicPointerCast(UniqueType<RawType> &rawType) {
         RawType *pointer = rawType.release();
-        ReturnType *newPointer = dynamic_cast<ReturnType *>(pointer); // NOLINT
+        ReturnType *newPointer = dynamic_cast<ReturnType *>(pointer);// NOLINT
         return std::unique_ptr<ReturnType>(newPointer);
     }
-}
+}// namespace sese
