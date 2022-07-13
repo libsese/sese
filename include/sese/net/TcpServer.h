9b7e05c745372e83db17d79f941490883c3f88f3
kaoru <SHIINA_KAORU@OUTLOOK.COM>
2022-07-14 01:06:57 +0800

    重写 Linux 下的 TcpServer

--- include/sese/net/TcpServer.h
+++ include/sese/net/TcpServer.h
@@ -8,3 +8,62 @@
 #include <sese/net/Socket.h>
 #include <sese/thread/ThreadPool.h>
 #include <sese/Noncopyable.h>
+#include <sese/Timer.h>
+#include <map>
+
+#ifdef __linux__
+#include <sys/epoll.h>
+#include <sese/ObjectPool.h>
+#define MaxEvents 64
+#define EpollCreate epoll_create1
+#define EpollWait epoll_wait
+#define EpollCtl epoll_ctl
+#define EpollEvent epoll_event
+#endif
+
+namespace sese {
+    class TcpServer;
+    class IOContext final : public Stream {
+    public:
+        friend class TcpServer;
+        int64_t read(void *buffer, size_t length) override;
+        int64_t write(const void *buffer, size_t length) override;
+        void close() override;
+
+#ifdef __linux__
+    private:
+        bool isClosed = false;
+        socket_t socket = -1;
+#endif
+    };
+
+    class TcpServer final : public Noncopyable {
+    public:
+        using Ptr = std::unique_ptr<TcpServer>;
+        using Map = std::map<socket_t, TimerTask::Ptr>;
+
+        static TcpServer::Ptr create(const IPAddress::Ptr &ipAddress, size_t threads, size_t keepAlive) noexcept;
+        void loopWith(const std::function<void(IOContext *)> &handler) noexcept;
+        void shutdown() noexcept;
+
+    private:
+        TcpServer() noexcept = default;
+        void closeCallback(socket_t socket) noexcept;
+
+        std::atomic_bool isShutdown = false;
+        ThreadPool::Ptr threadPool = nullptr;
+
+#ifdef __linux__
+    private:
+        int32_t epollFd = -1;
+        socket_t sockFd = -1;
+        epoll_event events[MaxEvents]{};
+        ObjectPool<IOContext>::Ptr ioContextPool = nullptr;
+
+        std::mutex mutex;
+        Map taskMap;
+        size_t keepAlive = 0;
+        Timer::Ptr timer = nullptr;
+#endif
+    };
+}// namespace sese
--- test/TestTcpServer/main.cpp
+++ test/TestTcpServer/main.cpp
@@ -1,17 +1,41 @@
 #include <sese/net/TcpServer.h>
 #include <sese/record/LogHelper.h>
 #include <sese/Test.h>
+#include <sese/Util.h>
 
-using sese::TcpServer;
 using sese::IOContext;
 using sese::IPv4Address;
 using sese::LogHelper;
+using sese::Socket;
+using sese::TcpServer;
 using sese::Test;
 using sese::Thread;
-using sese::Socket;
 
-LogHelper helper("fTCP_SERVER"); // NOLINT
+LogHelper helper("fTCP_SERVER");// NOLINT
 
 int main() {
+    auto address = IPv4Address::create("0.0.0.0", 8080);
+
+    auto server = TcpServer::create(address, 4, 30);
+    assert(helper, nullptr != server, -1);
+
+    // 服务器线程
+    Thread thread([&server]() {
+        server->loopWith([&helper = helper](IOContext *ioContext) {
+            char buffer[1024]{};
+            ioContext->read(buffer, 1024);
+            helper.info(buffer);
+            ioContext->write("HTTP/1.1 200 OK\r\n"
+                             "Host: www.sese.com:8080\r\n"
+                             "Connection: Keep-alive\r\n"
+                             "Content-Length: 0\r\n\r\n", 17 + 25 + 21 + 19 + 5);
+        });
+    },
+                  "TcpServer");
+    thread.start();
+    sese::sleep(5);
+    puts("WANT SHUTDOWN");
+    server->shutdown();
+    thread.join();
     return 0;
 }
