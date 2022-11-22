d165483d2308b76f93f609759891b174b8a713c6
Kaoru <SHIINA_KAORU@outlook.com>
2022-11-22 03:45:36 +0000

    codespaces 依赖测试

--- test/TestSqliteDriver.cpp
+++ test/TestSqliteDriver.cpp
@@ -9,7 +9,7 @@ int main () {
 
     sqlite3 *conn;
     sqlite3_open_v2("db_test.db", &conn, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX | SQLITE_OPEN_SHAREDCACHE , nullptr);
-    sqlite3_get_table(conn, "select user.name from db_user user where id = 1;", &table, &rows, &columns, &error);
+    sqlite3_get_table(conn, "select user.name from tb_user user where user.id = 1;", &table, &rows, &columns, &error);
     puts(table[1]);
 
     sqlite3_free_table(table);
