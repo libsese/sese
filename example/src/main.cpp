ef77a6fc3b9c3b66c782528aa9d0fe334439f265
Kaoru <SHIINA_KAORU@outlook.com>
2022-12-11 09:46:58 +0800

    update example

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,8 +1,18 @@
+#include <sese/Initializer.h>
 #include <sese/record/LogHelper.h>
 
-sese::LogHelper helper("example"); // NOLINT
+using sese::Initializer;
+using sese::record::LogHelper;
 
 int main() {
+    // 程序使用静态链接时，需要手动进行初始化；
+    // 使用动态链接时，手动进行初始化也无妨。
+    Initializer::getInitializer();
+
+    LogHelper helper("example");// NOLINT
     helper.info("This message sent by sese::LogHelper!");
+
+    // do something
+
     return 0;
 }
