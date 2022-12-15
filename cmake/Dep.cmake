74942844e7e694e917bfb83ed45163ba1fa0e274
kaoru <shiina_kaoru@outlook.com>
2022-12-15 10:58:57 +0800

    引入新依赖

--- cmake/Dep.cmake
+++ cmake/Dep.cmake
@@ -5,3 +5,14 @@ FetchContent_Declare(SString
         GIT_TAG v0.1.2
         )
 FetchContent_MakeAvailable(SString)
+
+option(LIBRESSL_TESTS "" off)
+option(LIBRESSL_APPS "" off)
+option(BUILD_SHARED_LIBS "" on)
+FetchContent_Declare(LibreSSL
+        GIT_REPOSITORY https://github.com/PowerShell/LibreSSL.git
+        GIT_TAG V3.6.1.0
+        )
+FetchContent_MakeAvailable(LibreSSL)
+target_include_directories(ssl PUBLIC ${LibreSSL_SOURCE_DIR}/include)
+target_include_directories(crypto PUBLIC ${LibreSSL_SOURCE_DIR})
