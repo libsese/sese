1894ebb19b9dc89839e44f7eb26649eaf1dd2b00
kaoru <shiina_kaoru@outlook.com>
2022-12-16 14:54:59 +0800

    依赖复用

--- cmake/Dep.cmake
+++ cmake/Dep.cmake
@@ -3,8 +3,12 @@ include(FetchContent)
 FetchContent_Declare(SString
         GIT_REPOSITORY https://github.com/SHIINASAMA/SString.git
         GIT_TAG v0.1.2
+        SOURCE_DIR ${CMAKE_SOURCE_DIR}/lib/SString
         )
-FetchContent_MakeAvailable(SString)
+FetchContent_GetProperties(SString)
+if(NOT SString_POPULATED)
+    FetchContent_MakeAvailable(SString)
+endif()
 
 if (SESE_USE_SSL)
     option(LIBRESSL_TESTS "" off)
@@ -13,8 +17,24 @@ if (SESE_USE_SSL)
     FetchContent_Declare(LibreSSL
             GIT_REPOSITORY https://github.com/PowerShell/LibreSSL.git
             GIT_TAG V3.6.1.0
+            SOURCE_DIR ${CMAKE_SOURCE_DIR}/lib/LibreSSL
             )
-    FetchContent_MakeAvailable(LibreSSL)
+    FetchContent_GetProperties(LibreSSL)
+    if(NOT LibreSSL_POPULATED)
+        FetchContent_MakeAvailable(LibreSSL)
+    endif()
     target_include_directories(ssl PUBLIC ${LibreSSL_SOURCE_DIR}/include)
     target_include_directories(crypto PUBLIC ${LibreSSL_SOURCE_DIR})
+endif ()
+
+if (SESE_USE_DB)
+    FetchContent_Declare(sese-db
+            GIT_REPOSITORY https://github.com/shiinasama/sese-db.git
+            GIT_TAG a885c07a24cb7bd2895017ae67456caaa6df0486
+            SOURCE_DIR ${CMAKE_SOURCE_DIR}/lib/sese-db
+            )
+    FetchContent_GetProperties(sese-db)
+    if(NOT sese-db_POPULATED)
+        FetchContent_MakeAvailable(sese-db)
+    endif()
 endif ()
