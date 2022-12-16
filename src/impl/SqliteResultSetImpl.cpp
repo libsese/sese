2a3027639819d6e82c7f09443db6c0e2a14cc0d6
kaoru <shiina_kaoru@outlook.com>
2022-12-16 16:10:34 +0800

    fix bug

--- src/impl/SqliteResultSetImpl.cpp
+++ src/impl/SqliteResultSetImpl.cpp
@@ -27,39 +27,36 @@ bool impl::SqliteResultSetImpl::next() noexcept {
     }
 }
 
-//const char *impl::SqliteResultSetImpl::getColumnByIndex(size_t index) const noexcept {
-//    return table[(rows + 1) * current + index];
-//}
 
 size_t impl::SqliteResultSetImpl::getColumns() const noexcept {
     return columns;
 }
 
 int32_t impl::SqliteResultSetImpl::getInteger(size_t index) const noexcept {
-    char *p = table[(rows + 1) * current + index];
+    char *p = table[(columns) * current + index];
     char *end;
     return std::strtol(p, &end, 10);
 }
 
 std::string_view impl::SqliteResultSetImpl::getString(size_t index) const noexcept {
-    char *p = table[(rows + 1) * current + index];
+    char *p = table[(columns) * current + index];
     return {p};
 }
 
 double impl::SqliteResultSetImpl::getDouble(size_t index) const noexcept {
-    char *p = table[(rows + 1) * current + index];
+    char *p = table[(columns) * current + index];
     char *end;
     return std::strtod(p, &end);
 }
 
 float impl::SqliteResultSetImpl::getFloat(size_t index) const noexcept {
-    char *p = table[(rows + 1) * current + index];
+    char *p = table[(columns) * current + index];
     char *end;
     return std::strtof(p, &end);
 }
 
 int64_t impl::SqliteResultSetImpl::getLong(size_t index) const noexcept {
-    char *p = table[(rows + 1) * current + index];
+    char *p = table[(columns) * current + index];
     char *end;
     return std::strtoll(p, &end, 10);
 }
