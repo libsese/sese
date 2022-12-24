ccf5394dfc498635ec6b80ff059d8000309414d5
kaoru <shiina_kaoru@outlook.com>
2022-12-24 12:41:14 +0800

    引入 SimpleUuid

--- cmake/Dep.cmake
+++ cmake/Dep.cmake
@@ -2,6 +2,8 @@ include(${PROJECT_SOURCE_DIR}/cmake/FetchContentToolKits.cmake)
 
 requires_from_git(SString https://github.com/SHIINASAMA/SString.git v0.1.2)
 
+requires_from_git(SimpleUuid https://github.com/SHIINASAMA/SimpleUuid.git v0.1)
+
 if (SESE_USE_SSL)
     option(LIBRESSL_TESTS "" off)
     option(LIBRESSL_APPS "" off)
