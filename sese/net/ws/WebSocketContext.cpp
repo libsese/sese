aa211f4a2d76e0513e006f13e7fa803fe26bdd97
kaoru <shiina_kaoru@outlook.com>
2023-01-04 17:25:24 +0800

    WebSocketContext

---
+++ sese/net/ws/WebSocketContext.cpp
@@ -0,0 +1,248 @@
+#include "sese/net/ws/WebSocketContext.h"
+#include "sese/util/Endian.h"
+
+sese::net::ws::WebSocketContext::WebSocketContext(sese::Stream *stream) noexcept {
+    this->stream = stream;
+}
+
+int64_t sese::net::ws::WebSocketContext::read(void *buffer, size_t length) {
+    // todo 应当改为使用循环
+    size_t readable;
+    if (length > info.length - readed) {
+        // 当前帧不满足读取
+        readable = info.length;
+        size_t read = autoRead(buffer, readable);
+        readed += read;
+
+        if (info.fin) {
+            // 已经是最后一帧，无法继续读取
+            return (int64_t) read;
+        }
+
+        // 尝试获取下一帧相关信息
+        if (readInfo()) {
+            // 获取到下一帧信息
+            return (int64_t) (read + this->read(((char *) buffer + read), length - read));
+        } else {
+            // 获取下一帧信息失败
+            return (int64_t) read;
+        }
+    } else {
+        // 当前帧能满足读取
+        readable = length;
+        size_t read = autoRead(buffer, readable);
+        readed += read;
+        return (int64_t) read;
+    }
+}
+
+int64_t sese::net::ws::WebSocketContext::write(const void *buffer, size_t length) {
+    int64_t total = 0;
+    FrameHeaderInfo header;
+    header.opCode = SESE_WS_OPCODE_TEXT;
+    auto p = (const char *)buffer;
+    while(length > 1024) {
+        header.length = 1024;
+        if(!writeInfo(header)) {
+            return -1;
+        }
+        auto rt = stream->write(p + total, 1024);
+        if (rt <= 0) {
+            return -1;
+        }
+
+        total += rt;
+        length -= rt;
+        header.opCode = SESE_WS_OPCODE_CONT;
+    }
+
+    if (length == 0) {
+        return total;
+    }
+    if(!writeInfo(header)) {
+        return -1;
+    }
+    auto rt = stream->write(p + total, length);
+    if (rt <= 0) {
+        return -1;
+    } else {
+        total += rt;
+        return total;
+    }
+}
+
+int64_t sese::net::ws::WebSocketContext::writeRaw(const void *buffer, size_t length) noexcept {
+    return stream->write(buffer, length);
+}
+
+void sese::net::ws::WebSocketContext::close() {
+    stream->close();
+}
+
+bool sese::net::ws::WebSocketContext::readInfo() noexcept {
+    struct FrameHeaderBuffer buffer;
+    if (this->read(&buffer, sizeof(buffer)) != sizeof(buffer)) {
+        return false;
+    }
+
+    info.fin = buffer.FIN;
+    info.rsv1 = buffer.RSV1;
+    info.rsv2 = buffer.RSV2;
+    info.rsv3 = buffer.RSV3;
+    info.mask = buffer.MASK;
+    info.opCode = buffer.OPCODE;
+
+    if (buffer.PAYLOAD_LEN <= 125) {
+        info.length = buffer.PAYLOAD_LEN;
+    } else if (buffer.PAYLOAD_LEN == 126) {
+        uint16_t len;
+        if (this->read(&len, sizeof(len)) != sizeof(len)) {
+            return false;
+        }
+        info.length = FromBigEndian16(len);
+    } else {
+        uint64_t len;
+        if (this->read(&len, sizeof(len)) != sizeof(len)) {
+            return false;
+        }
+        info.length = FromBigEndian64(len);
+    }
+
+    if (info.mask) {
+        if (this->read(&info.maskingKey, sizeof(info.maskingKey)) != sizeof(info.maskingKey)) {
+            return false;
+        }
+        // todo 验证大小端转换在此处是否是多余的
+        info.maskingKey = FromBigEndian32(info.maskingKey);
+
+        autoRead = [this](void *buf, size_t len) -> int64_t { return this->readWithMasking(info.maskingKey, buf, len); };
+    } else {
+        autoRead = [this](void *buf, size_t len) -> int64_t { return this->read(buf, len); };
+    }
+
+    return true;
+    // Payload data continued
+}
+
+bool sese::net::ws::WebSocketContext::writeInfo(const FrameHeaderInfo &info) noexcept {
+    struct FrameHeaderBuffer buffer;
+    buffer.FIN = info.fin;
+    buffer.RSV1 = info.rsv1;
+    buffer.RSV2 = info.rsv2;
+    buffer.RSV3 = info.rsv3;
+    buffer.MASK = info.mask;
+    buffer.OPCODE = info.opCode;
+
+    if (info.length <= 125) {
+        buffer.PAYLOAD_LEN = info.length;
+        if (stream->write(&buffer, sizeof(buffer)) != sizeof(buffer)) {
+            return false;
+        }
+    } else if (info.length > 125 && info.length <= 0xFFFF) {
+        buffer.PAYLOAD_LEN = 126;
+        if (stream->write(&buffer, sizeof(buffer)) != sizeof(buffer)) {
+            return false;
+        }
+        uint16_t len = ToBigEndian16(info.length);
+        if (stream->write(&len, sizeof(uint16_t)) != sizeof(uint16_t)) {
+            return false;
+        }
+    } else {
+        buffer.PAYLOAD_LEN = 127;
+        if (stream->write(&buffer, sizeof(buffer)) != sizeof(buffer)) {
+            return false;
+        }
+        uint64_t len = ToBigEndian64(info.length);
+        if (stream->write(&len, sizeof(uint64_t)) != sizeof(uint64_t)) {
+            return false;
+        }
+    }
+
+    if (info.mask) {
+        // todo 验证大小端转换在此处是否是多余的
+        uint32_t key = ToBigEndian32(info.maskingKey);
+        if (stream->write(&key, sizeof(uint32_t)) != sizeof(info.maskingKey)) {
+            return false;
+        }
+    }
+
+    return true;
+    // Payload data continued
+}
+
+int64_t sese::net::ws::WebSocketContext::readWithMasking(uint32_t maskingKey, void *buffer, size_t len) noexcept {
+    uint8_t temp[1024];
+    auto readLength = stream->read(temp, len);
+    if (readLength > 0) {
+        for (int i = 0; i < readLength; ++i) {
+            ((uint8_t *) buffer)[i] = temp[i] ^ ((const uint8_t *) &maskingKey)[i % 4];
+        }
+    }
+    return readLength;
+}
+
+int64_t sese::net::ws::WebSocketContext::writeWithMasking(uint32_t maskingKey, const void *buffer, size_t len) noexcept {
+    uint8_t temp[1024];
+    for (int i = 0; i < len; ++i) {
+        temp[i] = ((uint8_t *) buffer)[i] ^ ((const uint8_t *) &maskingKey)[i % 4];
+    }
+    return stream->write(temp, len);
+}
+
+bool sese::net::ws::WebSocketContext::ping() noexcept {
+    struct FrameHeaderBuffer buffer;
+    buffer.FIN = 1;
+    buffer.OPCODE = SESE_WS_OPCODE_PING;
+    return stream->write(&buffer, sizeof(buffer)) == sizeof(buffer);
+}
+
+bool sese::net::ws::WebSocketContext::pong() noexcept {
+    struct FrameHeaderBuffer buffer;
+    buffer.FIN = 1;
+    buffer.OPCODE = SESE_WS_OPCODE_PONG;
+    return stream->write(&buffer, sizeof(buffer)) == sizeof(buffer);
+}
+
+bool sese::net::ws::WebSocketContext::closeNoError() noexcept {
+    struct FrameHeaderBuffer buffer;
+    buffer.FIN = 1;
+    buffer.OPCODE = SESE_WS_OPCODE_CLOSE;
+    return stream->write(&buffer, sizeof(buffer)) == sizeof(buffer);
+}
+
+bool sese::net::ws::WebSocketContext::closeWithError(const void *error, size_t length) noexcept {
+    struct FrameHeaderInfo info;
+    info.fin = true;
+    info.opCode = SESE_WS_OPCODE_CLOSE;
+    if (error && length > 0) {
+        info.length = length;
+        if (!this->writeInfo(info)) {
+            return false;
+        }
+        return stream->write(error, length) == length;
+    } else {
+        return this->writeInfo(info);
+    }
+}
+
+bool sese::net::ws::WebSocketContext::closeWithError(const void *error, size_t length, uint32_t maskingKey) noexcept {
+    struct FrameHeaderInfo info;
+    info.fin = true;
+    info.opCode = SESE_WS_OPCODE_CLOSE;
+    if (error && length > 0) {
+        info.length = length;
+        info.mask = true;
+        info.maskingKey = maskingKey;
+        if (!this->writeInfo(info)) {
+            return false;
+        }
+
+        if (info.mask) {
+            return writeWithMasking(maskingKey, error, length) != length;
+        } else {
+            return stream->write(error, length) == length;
+        }
+    } else {
+        return this->writeInfo(info);
+    }
+}
--- sese/net/ws/WebSocketContext.h
+++ sese/net/ws/WebSocketContext.h
@@ -3,20 +3,38 @@
 #include "sese/net/ws/FrameHeader.h"
 #include "sese/util/Stream.h"
 
+#include <functional>
+
 namespace sese::net::ws {
 
     /// \brief 将普通流装饰为 WebSocket，仅用于处理协议的发送与部分接受处理
-    class API WebSocket : public Stream {
+    class API WebSocketContext : public Stream {
     public:
-        explicit WebSocket(Stream *stream) noexcept;
+        explicit WebSocketContext(Stream *stream) noexcept;
 
+        /// \brief 读取内容
+        /// \warning 此方法使用的递归，请注意 length 大小
+        /// \param buffer 缓存
+        /// \param length 需要读取的长度
+        /// \return 实际读取长度
         int64_t read(void *buffer, size_t length) override;
+        /// \brief 写入内容
+        /// \warning 此方法每调用一次都会至少产生一帧
+        /// \param buffer 缓存
+        /// \param length 需要写入的长度
+        /// \return 实际写入的长度
         int64_t write(const void *buffer, size_t length) override;
         void close() override;
 
     public:
-        bool readInfo(FrameHeaderInfo &info) noexcept;
+        bool readInfo() noexcept;
         bool writeInfo(const FrameHeaderInfo &info) noexcept;
+
+        /// 直接写入裸流，需要搭配 writeInfo 使用，可手动处理每一帧的大小
+        /// \param buffer 缓存指针
+        /// \param length 缓存大小
+        /// \return 实际写入字节数
+        int64_t writeRaw(const void *buffer, size_t length) noexcept;
 
         /// 读取反掩码后的数据（通常服务端用）
         /// \param buffer 缓存指针
@@ -52,6 +70,9 @@ namespace sese::net::ws {
         bool closeWithError(const void *error, size_t length, uint32_t maskingKey) noexcept;
 
     private:
-        Stream *stream = nullptr;
+        std::function<int64_t(void *, size_t)> autoRead;// 自动处理掩码的读取
+        Stream *stream = nullptr;                       // 被装饰的流
+        FrameHeaderInfo info;                           // 当前读取的帧头
+        uint64_t readed = 0;                            // 当前帧已读的长度
     };
 }// namespace sese::net::ws
