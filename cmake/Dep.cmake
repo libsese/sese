f7f404d928a9a49ae5920fd54e9522eb432cc0e8
kaoru <shiina_kaoru@outlook.com>
2023-01-13 02:02:04 +0800

    update some license files

--- cmake/Dep.cmake
+++ cmake/Dep.cmake
@@ -7,8 +7,13 @@ requires_from_git(SimpleUuid https://github.com/libsese/SimpleUuid.git v0.1.2)
 option(LIBRESSL_TESTS "" OFF)
 option(LIBRESSL_APPS "" OFF)
 option(LIBRESSL_SKIP_INSTALL "" ON)
-option(BUILD_SHARED_LIBS "" ON)
+option(BUILD_SHARED_LIBS "" OFF)
 requires_from_git(LibreSSL https://github.com/PowerShell/LibreSSL.git V3.6.1.0)
-target_include_directories(ssl PUBLIC ${LibreSSL_SOURCE_DIR}/include)
-# 此选项会导致在 Darwin 下与同名文件 VSERION 冲突，若有需要请尝试从更高一层的目录进行访问
+target_include_directories(ssl-static PUBLIC ${LibreSSL_SOURCE_DIR}/include)
+# 此选项会导致在 Darwin 下与同名文件 VERSION 冲突，若有需要请尝试从更高一层的目录进行访问
 # target_include_directories(crypto PUBLIC ${LibreSSL_SOURCE_DIR})
+if (MSVC)
+    # 屏蔽不需要关注的目标的警告
+    target_compile_options(crypto_obj PRIVATE /wd4061 /wd4065 /wd4152 /wd4132 /wd4191 /wd4242 /wd4255 /wd4295 /wd4296 /wd4388 /wd4464 /wd4668 /wd4701 /wd4702 /wd4774 /wd4819 /wd4820 /wd5045 /wd5105)
+    target_compile_options(ssl_obj PRIVATE /wd4061 /wd4065 /wd4152 /wd4132 /wd4191 /wd4242 /wd4255 /wd4295 /wd4296 /wd4388 /wd4464 /wd4668 /wd4701 /wd4702 /wd4774 /wd4819 /wd4820 /wd5045 /wd5105)
+endif ()
