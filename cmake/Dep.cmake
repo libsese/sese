59416fb44c73bbb797d596f02c1fb3d799662eed
Kaoru <SHIINA_KAORU@outlook.com>
2023-01-18 00:43:26 +0800

    修复 Windows 下的编译错误，简化添加测试流程

--- cmake/Dep.cmake
+++ cmake/Dep.cmake
@@ -7,7 +7,7 @@ requires_from_git(SimpleUuid https://github.com/libsese/SimpleUuid.git v0.1.2)
 option(LIBRESSL_TESTS "" OFF)
 option(LIBRESSL_APPS "" OFF)
 option(LIBRESSL_SKIP_INSTALL "" ON)
-option(BUILD_SHARED_LIBS "" OFF)
+option(BUILD_SHARED_LIBS "" ON)
 requires_from_git(LibreSSL https://github.com/PowerShell/LibreSSL.git V3.6.1.0)
 # target_include_directories(ssl-static PUBLIC ${LibreSSL_SOURCE_DIR}/include)
 # 此选项会导致在 Darwin 下与同名文件 VERSION 冲突，若有需要请尝试从更高一层的目录进行访问
