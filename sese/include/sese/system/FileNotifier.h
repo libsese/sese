f83ece2f59664aef6aa60cffcf74368e28b0055f
Kaoru <SHIINA_KAORU@outlook.com>
2023-03-10 22:36:35 +0800

    FileNotifier Linux 实现

--- sese/include/sese/system/FileNotifier.h
+++ sese/include/sese/system/FileNotifier.h
@@ -3,24 +3,26 @@
 #include "sese/Config.h"
 #include "sese/record/LogHelper.h"
 
+#include <atomic>
+
 namespace sese {
 
     struct API FileNotifyOption {
         // 创建文件
-        virtual void onCreate(const std::string &path) {
-            sese::record::LogHelper::d("CREATE %s", path.c_str());
+        virtual void onCreate(std::string_view path) {
+            sese::record::LogHelper::d("CREATE %s", path.data());
         };
         // 移动文件
-        virtual void onMove(const std::string &srcPath, const std::string &dstPath) {
-            sese::record::LogHelper::d("MOVE %s -> %s", srcPath.c_str(), dstPath.c_str());
+        virtual void onMove(std::string_view srcPath, std::string_view dstPath) {
+            sese::record::LogHelper::d("MOVE %s -> %s", srcPath.data(), dstPath.data());
         };
         // 修改文件
-        virtual void onModify(const std::string &path) {
-            sese::record::LogHelper::d("MODIFY %s", path.c_str());
+        virtual void onModify(std::string_view path) {
+            sese::record::LogHelper::d("MODIFY %s", path.data());
         };
         // 删除文件
-        virtual void onDelete(const std::string &path) {
-            sese::record::LogHelper::d("DELETE %s", path.c_str());
+        virtual void onDelete(std::string_view path) {
+            sese::record::LogHelper::d("DELETE %s", path.data());
         };
     };
 
@@ -41,6 +43,10 @@ namespace sese {
 #ifdef WIN32
         void *fileHandle = nullptr;
         void *overlapped = nullptr;
+        std::atomic_bool isShutdown = false;
+#elif __linux__
+        int inotifyFd = -1;
+        int watchFd = -1;
         std::atomic_bool isShutdown = false;
 #endif
     };
---
+++ sese/src/native/Linux/system/FileNotifier.cpp
@@ -0,0 +1,85 @@
+#include "sese/system/FileNotifier.h"
+#include "sese/record/LogHelper.h"
+
+#include <sys/inotify.h>
+#include <sys/fcntl.h>
+
+sese::FileNotifier::Ptr sese::FileNotifier::create(const std::string &path, FileNotifyOption *option) noexcept {
+    int inotifyFd = inotify_init1(0);
+    if (inotifyFd < 0) {
+        return nullptr;
+    }
+
+    int opt = fcntl(inotifyFd, F_GETFL);
+    if (opt < 0) {
+        close(inotifyFd);
+        return nullptr;
+    }
+
+    int ret = fcntl(inotifyFd, F_SETFL, opt | O_NONBLOCK);
+    if (ret < 0) {
+        close(inotifyFd);
+        return nullptr;
+    }
+
+    int watchFd = inotify_add_watch(inotifyFd, path.c_str(), IN_MODIFY | IN_CREATE | IN_DELETE | IN_MOVE);
+    if (watchFd < 0) {
+        close(inotifyFd);
+        return nullptr;
+    }
+
+    auto notifier = new FileNotifier;
+    notifier->option = option;
+    notifier->inotifyFd = inotifyFd;
+    notifier->watchFd = watchFd;
+    return std::unique_ptr<FileNotifier>(notifier);
+}
+
+void sese::FileNotifier::loop() noexcept {
+    fd_set fdSet;
+    struct timeval timeout {
+        1, 0
+    };
+    char buffer[1024];
+    while (!isShutdown) {
+        FD_ZERO(&fdSet);
+        FD_SET(inotifyFd, &fdSet);
+        select(FD_SETSIZE, &fdSet, nullptr, nullptr, &timeout);
+        if (FD_ISSET(inotifyFd, &fdSet)) {
+            auto pEvent = (inotify_event *) &buffer;
+            auto len = read(inotifyFd, buffer, sizeof(buffer));
+            auto times = (len / sizeof(inotify_event)) - 1;
+            char *fromString = nullptr;
+            while (times) {
+                if (pEvent->wd != watchFd) {
+                    pEvent++;
+                    times--;
+                    continue;
+                } else {
+                    if (pEvent->mask & IN_CREATE) {
+                        option->onCreate({pEvent->name});
+                    } else if (pEvent->mask & IN_MODIFY) {
+                        option->onModify({pEvent->name});
+                    } else if (pEvent->mask & IN_DELETE) {
+                        option->onDelete({pEvent->name});
+                    } else if (pEvent->mask & IN_MOVED_FROM) {
+                        fromString = pEvent->name;
+                    } else if (pEvent->mask & IN_MOVED_TO) {
+                        if (fromString) {
+                            option->onMove({fromString}, {pEvent->name});
+                            fromString = nullptr;
+                        }
+                    }
+                    times--;
+                    pEvent++;
+                }
+            }
+        }
+    }
+}
+
+void sese::FileNotifier::shutdown() noexcept {
+    isShutdown = true;
+    close(watchFd);
+    close(inotifyFd);
+}
