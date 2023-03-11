5a22506071c01deec3143efaaaf7d7043786cebe
kaoru <shiina_kaoru@outlook.com>
2023-03-11 05:37:54 -0800

    FileNotifier Darwin 实现

--- sese/include/sese/system/FileNotifier.h
+++ sese/include/sese/system/FileNotifier.h
@@ -4,6 +4,9 @@
 #include "sese/record/LogHelper.h"
 
 #include <atomic>
+#ifdef __APPLE__
+#include <tuple>
+#endif
 
 namespace sese {
 
@@ -12,14 +15,17 @@ namespace sese {
         virtual void onCreate(std::string_view path) {
             sese::record::LogHelper::d("CREATE %s", path.data());
         };
+
         // 移动文件
         virtual void onMove(std::string_view srcPath, std::string_view dstPath) {
             sese::record::LogHelper::d("MOVE %s -> %s", srcPath.data(), dstPath.data());
         };
+
         // 修改文件
         virtual void onModify(std::string_view path) {
             sese::record::LogHelper::d("MODIFY %s", path.data());
         };
+
         // 删除文件
         virtual void onDelete(std::string_view path) {
             sese::record::LogHelper::d("DELETE %s", path.data());
@@ -32,22 +38,27 @@ namespace sese {
 
         static FileNotifier::Ptr create(const std::string &path, FileNotifyOption *option) noexcept;
 
-        void loop() noexcept;
+        void loopNonblocking() noexcept;
 
         void shutdown() noexcept;
 
     private:
         FileNotifier() = default;
 
-        FileNotifyOption *option = nullptr;
 #ifdef WIN32
         void *fileHandle = nullptr;
         void *overlapped = nullptr;
         std::atomic_bool isShutdown = false;
+        FileNotifyOption *option = nullptr;
 #elif __linux__
         int inotifyFd = -1;
         int watchFd = -1;
         std::atomic_bool isShutdown = false;
+        FileNotifyOption *option = nullptr;
+#elif __APPLE__
+        void *stream = nullptr;
+        void *queue = nullptr;
+        std::tuple<size_t, FileNotifyOption *> lenAndOption;
 #endif
     };
 }// namespace sese
---
+++ sese/src/native/Darwin/system/FileNotifier.cpp
@@ -0,0 +1,94 @@
+#include "sese/system/FileNotifier.h"
+
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreServices/CoreServices.h>
+#include <tuple>
+
+static void callback(
+        ConstFSEventStreamRef stream,
+        void *info, 
+        size_t numEvents,
+        void *eventPaths,
+        const FSEventStreamEventFlags eventFlags[],
+        const FSEventStreamEventId eventIds[]
+) {
+    auto tuple = *(std::tuple<size_t, sese::FileNotifyOption *> *) info;
+    auto len = std::get<0>(tuple) + 1;
+    auto option = (sese::FileNotifyOption *) std::get<1>(tuple);
+    const char *lastSrc = nullptr;
+    for (size_t i = 0; i < numEvents; ++i) {
+        auto dict = (CFDictionaryRef) CFArrayGetValueAtIndex((CFArrayRef) eventPaths, (CFIndex) i);
+        auto path = (CFStringRef) CFDictionaryGetValue(dict, kFSEventStreamEventExtendedDataPathKey);
+        auto src = CFStringGetCStringPtr(path, kCFStringEncodingUTF8);
+        if (eventFlags[i] & kFSEventStreamEventFlagItemRemoved) {
+            option->onDelete(src + len);
+        } else if (eventFlags[i] & kFSEventStreamEventFlagItemCreated) {
+            option->onCreate(src + len);
+        } else if (eventFlags[i] & kFSEventStreamEventFlagItemModified) {
+            option->onModify(src + len);
+        } else if (eventFlags[i] & kFSEventStreamEventFlagItemRenamed) {
+            if (lastSrc) {
+                option->onMove(lastSrc + len, src + len);
+                lastSrc = nullptr;
+            } else {
+                lastSrc = src;
+            }
+        }
+    }
+}
+
+sese::FileNotifier::Ptr sese::FileNotifier::create(const std::string &path, FileNotifyOption *option) noexcept {
+    CFStringRef p = CFStringCreateWithCString(
+            nullptr,
+            path.c_str(),
+            kCFStringEncodingUTF8
+    );
+
+    CFArrayRef pathsToWatch = CFArrayCreate(
+            nullptr,
+            (const void **) &p,
+            1,
+            &kCFTypeArrayCallBacks
+    );
+
+    auto notifier = new FileNotifier;
+    notifier->lenAndOption = std::tuple<size_t, FileNotifyOption *>(path.length(), option);
+
+    FSEventStreamContext context;
+    context.version = 0;
+    context.info = &notifier->lenAndOption;
+    context.retain = nullptr;
+    context.release = nullptr;
+    context.copyDescription = nullptr;
+
+    notifier->stream = FSEventStreamCreate(
+            nullptr,
+            &callback,
+            &context,
+            pathsToWatch,
+            kFSEventStreamEventIdSinceNow,
+            1,
+            kFSEventStreamCreateFlagFileEvents | kFSEventStreamCreateFlagUseCFTypes | kFSEventStreamCreateFlagUseExtendedData
+    );
+
+    notifier->queue = dispatch_queue_create(nullptr, nullptr);
+    FSEventStreamSetDispatchQueue(
+            (FSEventStreamRef) notifier->stream,
+            (dispatch_queue_t) notifier->queue
+    );
+
+    return std::unique_ptr<FileNotifier>(notifier);
+}
+
+void sese::FileNotifier::loopNonblocking() noexcept {
+    FSEventStreamStart((FSEventStreamRef) this->stream);
+}
+
+void sese::FileNotifier::shutdown() noexcept {
+    FSEventStreamStop((FSEventStreamRef) this->stream);
+    FSEventStreamInvalidate((FSEventStreamRef) this->stream);
+    FSEventStreamRelease((FSEventStreamRef) this->stream);
+    dispatch_release((dispatch_queue_t) this->queue);
+    stream = nullptr;
+    queue = nullptr;
+}
