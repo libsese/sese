d4196ad4a7d582a1355039faf6c5f86dc8e76d47
Kaoru <SHIINA_KAORU@outlook.com>
2023-03-11 22:30:52 +0800

    FileNotifier Docs

--- sese/include/sese/system/FileNotifier.h
+++ sese/include/sese/system/FileNotifier.h
@@ -1,7 +1,12 @@
+/// \file FileNotifier.h
+/// \version 0.1
+/// \author kaoru
+/// \date 2023年3月11日
+/// \brief 文件变更监视器
+
 #pragma once
 
 #include "sese/Config.h"
-#include "sese/record/LogHelper.h"
 
 #include <atomic>
 #ifdef __APPLE__
@@ -13,35 +18,34 @@
 namespace sese {
 
     struct API FileNotifyOption {
-        // 创建文件
-        virtual void onCreate(std::string_view path) {
-            sese::record::LogHelper::d("CREATE %s", path.data());
-        };
+        /// 创建文件
+        virtual void onCreate(std::string_view path){};
 
-        // 移动文件
-        virtual void onMove(std::string_view srcPath, std::string_view dstPath) {
-            sese::record::LogHelper::d("MOVE %s -> %s", srcPath.data(), dstPath.data());
-        };
+        /// 移动文件
+        virtual void onMove(std::string_view srcPath, std::string_view dstPath){};
 
-        // 修改文件
-        virtual void onModify(std::string_view path) {
-            sese::record::LogHelper::d("MODIFY %s", path.data());
-        };
+        /// 修改文件
+        virtual void onModify(std::string_view path){};
 
-        // 删除文件
-        virtual void onDelete(std::string_view path) {
-            sese::record::LogHelper::d("DELETE %s", path.data());
-        };
+        /// 删除文件
+        virtual void onDelete(std::string_view path){};
     };
 
+    /// 文件变更监视器，此实现在不同平台下行为细节不一致，过于复杂的应用请不要优先考虑使用此工具类
     class API FileNotifier {
     public:
         using Ptr = std::unique_ptr<FileNotifier>;
 
+        /// 创建文件监视器
+        /// \param path 此处应为[文件夹]的路径
+        /// \param option 回调选项
+        /// \retval nullptr 创建失败
         static FileNotifier::Ptr create(const std::string &path, FileNotifyOption *option) noexcept;
 
+        /// 启动线程，开始处理变更事件
         void loopNonblocking() noexcept;
 
+        /// 关闭监视器并阻塞至后台线程退出
         void shutdown() noexcept;
 
     private:
