55db3c1ea96a5e2aa7f6034e0b26e7e8885dabc9
Kaoru <SHIINA_KAORU@outlook.com>
2023-03-15 21:51:15 +0800

    docs: 添加文档

--- sese/include/sese/convert/GZipFileInputStream.h
+++ sese/include/sese/convert/GZipFileInputStream.h
@@ -1,13 +1,23 @@
+/// \file GZipFileInputStream.h
+/// \author kaoru
+/// \brief GZip 文件输入流（解压）
+/// \version 0.1
+/// \date 2023年3月15日
+
 #pragma once
 
 #include "sese/util/InputStream.h"
 
 namespace sese {
 
+    /// GZip 文件输入流（解压）
     class API GZipFileInputStream : public InputStream {
     public:
         using Ptr = std::unique_ptr<GZipFileInputStream>;
 
+        /// 创建文件输入流对象
+        /// \param file 文件的相对或者绝对路径
+        /// \retval nullptr 打开文件失败
         static GZipFileInputStream::Ptr create(const char *file) noexcept;
 
         int64_t read(void *buffer, size_t length) override;
--- sese/include/sese/convert/GZipFileOutputStream.h
+++ sese/include/sese/convert/GZipFileOutputStream.h
@@ -1,13 +1,24 @@
+/// \file GZipFileOutputStream.h
+/// \author kaoru
+/// \brief GZip 文件输出流（压缩）
+/// \version 0.1
+/// \date 2023年3月15日
+
 #pragma once
 
 #include "sese/util/OutputStream.h"
 
 namespace sese {
 
+    /// GZip 文件输出流（压缩）
     class API GZipFileOutputStream : public OutputStream {
     public:
         using Ptr = std::unique_ptr<GZipFileOutputStream>;
 
+        /// 创建文件输出流对象
+        /// \param file 文件的相对或者绝对路径
+        /// \param level 压缩等级(取值范围为0~9，自动处理数值区间)
+        /// \retval nullptr 打开文件失败
         static GZipFileOutputStream::Ptr create(const char *file, size_t level) noexcept;
 
         int64_t write(const void *buffer, size_t length) override;
