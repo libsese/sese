2b00fe5f77a4d1469567747d4f4b9e9b6a8b3031
Kaoru <SHIINA_KAORU@outlook.com>
2023-03-30 08:46:46 +0000

    docs: CSV 工具文档完善

--- sese/include/sese/config/CSVReader.h
+++ sese/include/sese/config/CSVReader.h
@@ -1,3 +1,9 @@
+/// \file CSVReader.h
+/// \brief CSV 流读取器
+/// \author kaoru
+/// \version 0.1
+/// \date 2023年3月30日
+
 #pragma once
 
 #include "sese/util/InputStream.h"
@@ -7,12 +13,18 @@
 
 namespace sese {
 
+    /// CSV 流读取器
     class API CSVReader {
     public:
         using Row = std::vector<std::string>;
 
+        /// 构造函数
+        /// \param source 读取的流
+        /// \param splitChar 分割字符
         explicit CSVReader(InputStream *source, char splitChar = ',') noexcept;
 
+        /// 从流中读取一行
+        /// \return 返回包含一行内所有元素的 vector 容器
         Row read() noexcept;
 
     protected:
--- sese/include/sese/config/CSVWriter.h
+++ sese/include/sese/config/CSVWriter.h
@@ -1,3 +1,9 @@
+/// \file CSVReader.h
+/// \brief CSV 流写入器
+/// \author kaoru
+/// \version 0.1
+/// \date 2023年3月30日
+
 #pragma once
 
 #include "sese/util/OutputStream.h"
@@ -6,14 +12,21 @@
 #include <string>
 
 namespace sese {
+
+    /// CSV 流写入器
     class API CSVWriter {
     public:
         using Row = std::vector<std::string>;
 
         static const char *CRLF;
 
-        explicit CSVWriter(OutputStream *dest, char spiltChar = ',') noexcept;
+        /// 构造函数
+        /// \param dest 目的流
+        /// \param split 分割字符
+        explicit CSVWriter(OutputStream *dest, char splitChar = ',') noexcept;
 
+        /// 写入流
+        /// \param row 待写入行
         void write(const Row &row) noexcept;
 
     protected:
