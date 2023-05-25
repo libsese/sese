7bcc97a611465c1c3a235c613875423a1550f592
DreamerQcl <75739703+DreamerQcl@users.noreply.github.com>
2023-05-25 17:07:41 +0800

    refactor: 完成对TestMariaInstance.cpp和TestSqliteInstance.cpp的重构 但未经过实际测试

--- test/TestMariaInstance.cpp
+++ test/TestMariaInstance.cpp
@@ -6,107 +6,158 @@ using sese::db::DriverInstance;
 using sese::db::DriverManager;
 using sese::db::ResultSet;
 
-auto instance = DriverManager::getInstance(
-        DatabaseType::Maria,
-        "host=127.0.0.1;user=root;pwd=qiuchenli;db=db_test;port=3306;"
-);
-
-TEST(TestDriverInstance, TestInstance) {
-    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
-}
 
 // 查某行数据
 TEST(TestDriverInstance, TestQueryData) {
     auto instance = DriverManager::getInstance(
             DatabaseType::Maria,
-            "host=127.0.0.1;user=root;pwd=qiuchenli;db=db_test;port=3306;"
+            "host=127.0.0.1;user=root;pwd=libsese;db=db_test;port=3306;"
     );
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
 
-    auto result = instance->executeQuery("select name from tb_delete where id = 1;");
-    ASSERT_NE(nullptr, result) << "Query failure";
-    printf("result: id = %d name = %s\n", (int) result->getInteger(0), result->getString(1).data());
+    auto result = instance->executeQuery("select * from tb_query where id = 1");
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
+        printf("result id = %d name = %s\n", result->getInteger(0), result->getString(1).data());
+    }
 }
 
 // 改
 TEST(TestDriverInstance, TestModifyData) {
-    auto result = instance->executeQuery("update id from tb_update where id = 1;");
-    printf("result: id = %d name = %s\n", (int) result->getInteger(0), result->getString(1).data());
+    auto instance = DriverManager::getInstance(
+            DatabaseType::Maria,
+            "host=127.0.0.1;user=root;pwd=libsese;db=db_test;port=3306;"
+    );
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    auto result = instance->executeQuery("update * from tb_update where id = 1;");
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
+        printf("result: id = %d name = %s\n", (int) result->getInteger(0), result->getString(1).data());
+    };
 
     auto count = instance->executeUpdate("update tb_update set name = 'bar' where id = 1;");
     EXPECT_NE(-1, count) << "Update failure";
 
-    auto result1 = instance->executeQuery("select id from tb_update where id = 1;");
-    printf("result1: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+    auto result1 = instance->executeQuery("select * from tb_update where id = 1;");
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
+        printf("result1: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+    }
 }
 
 // 增
 TEST(TestDriverInstance, TestInsertData) {
+    auto instance = DriverManager::getInstance(
+            DatabaseType::Maria,
+            "host=127.0.0.1;user=root;pwd=libsese;db=db_test;port=3306;"
+    );
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
     auto result = instance->executeQuery("select * from tb_insert;");
+    EXPECT_NE(nullptr, result) << "Query failure";
     while (result->next()) {
         printf("result: id = %d name = %s\n", (int) result->getInteger(0), result->getString(1).data());
     }
 
-    auto count2 = instance->executeUpdate("insert into tb_insert (id, name) values(3, 'mike');");
-    EXPECT_NE(-1, count2) << "Insertion failure";
+    auto count = instance->executeUpdate("insert into tb_insert (id, name) values(3, 'mike');");
+    EXPECT_NE(-1, count) << "Insertion failure";
 
     auto result1 = instance->executeQuery("select * from tb_insert;");
+    EXPECT_NE(nullptr, result1) << "Query failure";
     while (result1->next()) {
-        printf("result: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+        printf("result1: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
     }
 }
 
 // 删
 TEST(TestDriverInstance, TestDeleteData) {
+    auto instance = DriverManager::getInstance(
+            DatabaseType::Maria,
+            "host=127.0.0.1;user=root;pwd=libsese;db=db_test;port=3306;"
+    );
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
     auto result = instance->executeQuery("select * from tb_delete;");
+    EXPECT_NE(nullptr, result) << "Query failure";
     while (result->next()) {
         printf("result: id = %d name = %s\n", (int) result->getInteger(0), result->getString(1).data());
     }
 
-    auto count3 = instance->executeUpdate("delete from tb_delete where id = 1;");
-    EXPECT_NE(-1, count3) << "Deletion failure";
+    auto count = instance->executeUpdate("delete from tb_delete where id = 1;");
+    EXPECT_NE(-1, count) << "Deletion failure";
 
     auto result1 = instance->executeQuery("select * from tb_delete;");
+    EXPECT_NE(nullptr, result1) << "Query failure";
     while (result1->next()) {
-        printf("result: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+        printf("result1: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
     }
 }
 
 // 创建查询的预处理语句
 TEST(TestCreateStmt, TestQueryStmt) {
-    int64_t value = 1;
-    auto stmt = instance->createStatement("select name from tb_stmt_query where id = ?;");
+    auto instance = DriverManager::getInstance(
+            DatabaseType::Maria,
+            "host=127.0.0.1;user=root;pwd=libsese;db=db_test;port=3306;"
+    );
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    int64_t id = 1;
+    auto stmt = instance->createStatement("select * from tb_stmt_query where id = ?;");
     ASSERT_NE(nullptr, stmt) << "Failed to create a query preprocessing statement";
 
-    EXPECT_EQ(true, stmt->setLong(1, value));
+    EXPECT_EQ(true, stmt->setLong(1, id)) << "Failed to fill in the Long value parameter";
 
     auto result = stmt->executeQuery();
     EXPECT_NE(nullptr, result) << "Failed to use the query prepared statement";
+    while (result->next()) {
+        printf("result: id = %d, name = %s\n", (int) result->getInteger(0), result->getString(1).data());
+    }
 }
 
 // 创建更新的预处理语句
-TEST(TestCreateStmt, TestUateStmt) {
+TEST(TestCreateStmt, TestUpdateStmt) {
+    auto instance = DriverManager::getInstance(
+            DatabaseType::Maria,
+            "host=127.0.0.1;user=root;pwd=libsese;db=db_test;port=3306;"
+    );
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
     auto result = instance->executeQuery("select * from tb_stmt_update where id = 1;");
-    printf("result: id = %d name = %s\n", (int) result->getInteger(0), result->getString(1).data());
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
+        printf("result: id = %d name = %s\n", (int) result->getInteger(0), result->getString(1).data());
+    }
 
     int64_t id = 1;
     const char *name = "mike";
-    auto stmt1 = instance->createStatement("update tb_stmt_update set name = ? where id = ?;");
-    ASSERT_NE(nullptr, stmt1) << "Failed to create update a preprocessed statement";
+    auto stmt = instance->createStatement("update tb_stmt_update set name = ? where id = ?;");
+    ASSERT_NE(nullptr, stmt) << "Failed to create update a preprocessed statement";
 
-    EXPECT_EQ(true, stmt1->setText(1, name)) << "Failed to fill in the text value parameter";
-    EXPECT_EQ(true, stmt1->setLong(2, id)) << "Failed to fill in the Long value parameter";
+    EXPECT_EQ(true, stmt->setText(1, name)) << "Failed to fill in the text value parameter";
+    EXPECT_EQ(true, stmt->setLong(2, id)) << "Failed to fill in the Long value parameter";
 
-    auto count = stmt1->executeUpdate();
+    auto count = stmt->executeUpdate();
     EXPECT_NE(-1, count) << "Failed to use the updated prepared statement";
 
     auto result1 = instance->executeQuery("select * from tb_stmt_update where id = 1;");
-    printf("result1: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
+        printf("result1: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+    }
 }
 
 // 创建删除的预处理语句
 TEST(TestCreateStmt, TestdeleteStmt) {
+    auto instance = DriverManager::getInstance(
+            DatabaseType::Maria,
+            "host=127.0.0.1;user=root;pwd=libsese;db=db_test;port=3306;"
+    );
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
     auto result = instance->executeQuery("select * from tb_stmt_delete;");
-    while (result->next()){
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
         printf("result: id = %d name = %s\n", (int) result->getInteger(0), result->getString(1).data());
     }
 
@@ -114,35 +165,47 @@ TEST(TestCreateStmt, TestdeleteStmt) {
     auto stmt = instance->createStatement("delete tb_stmt_delete where id = ?;");
     ASSERT_NE(nullptr, stmt) << "Failed to create delete a preprocessed statement";
 
-    EXPECT_EQ(true, stmt->setLong(2, id)) << "Failed to fill in the Long value parameter";
+    EXPECT_EQ(true, stmt->setLong(1, id)) << "Failed to fill in the Long value parameter";
 
     auto count = stmt->executeUpdate();
     EXPECT_NE(-1, count) << "Failed to use the delete prepared statement";
 
     auto result1 = instance->executeQuery("select * from tb_stmt_delete;");
-    while (result1->next()){
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
         printf("result1: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
     }
 }
 
 // 创建插入的预处理语句
 TEST(TestCreateStmt, TestinsertStmt) {
+    auto instance = DriverManager::getInstance(
+            DatabaseType::Maria,
+            "host=127.0.0.1;user=root;pwd=libsese;db=db_test;port=3306;"
+
+    );
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
     auto result = instance->executeQuery("select * from tb_stmt_insert;");
-    while (result->next()){
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
         printf("result: id = %d name = %s\n", (int) result->getInteger(0), result->getString(1).data());
     }
 
-    int64_t id = 1;
-    auto stmt = instance->createStatement("delete tb_stmt_insert where id = ?;");
+    int64_t id = 3;
+    const char *name = "mike";
+    auto stmt = instance->createStatement("insert into tb_stmt_insert (id, values) values (?, ?);");
     ASSERT_NE(nullptr, stmt) << "Failed to create insert a preprocessed statement";
 
-    EXPECT_EQ(true, stmt->setLong(2, id)) << "Failed to fill in the Long value parameter";
+    EXPECT_EQ(true, stmt->setLong(1, id)) << "Failed to fill in the Long value parameter";
+    EXPECT_EQ(true, stmt->setText(1, name)) << "Failed to fill in the Test value parameter";
 
     auto count = stmt->executeUpdate();
     EXPECT_NE(-1, count) << "Failed to use the insert prepared statement";
 
     auto result1 = instance->executeQuery("select * from tb_stmt_insert;");
-    while (result1->next()){
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
         printf("result1: id = %d name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
     }
 }
--- test/TestSqliteInstance.cpp
+++ test/TestSqliteInstance.cpp
@@ -1,36 +1,185 @@
 #include <sese/db/DriverManager.h>
+#include <gtest/gtest.h>
 
 using sese::db::DatabaseType;
 using sese::db::DriverInstance;
 using sese::db::DriverManager;
 using sese::db::ResultSet;
 
-int main() {
-    auto instance = DriverManager::getInstance(DatabaseType::Sqlite, "db_test.db");
-    {
-        auto result = instance->executeQuery("select user.id, user.name from tb_user user where id = 1;");
-        result->next();
+// query
+TEST(TestDriverInstance, TestQueryData) {
+    auto instance = DriverManager::getInstance(DatabaseType::Sqlite, "/root/db_test.db");
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    auto result = instance->executeQuery("select * from tb_query where id = 1;");
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
         printf("id = %d, name = %s\n", (int) result->getInteger(0), result->getString(1).data());
-        auto count = instance->executeUpdate(R"(update tb_user set name = "kaoru" where id = 1;)");
-        printf("update rows: %d\n", (int) count);
+    }
+}
+
+// update
+TEST(TestDriverInstance, TestUpdateData) {
+    auto instance = DriverManager::getInstance(DatabaseType::Sqlite, "/root/db_test.db");
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    auto result = instance->executeQuery("select * from tb_update where id = 1;");
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
+        printf("result id = %d, name = %s\n", (int) result->getInteger(0), result->getString(1).data());
     }
 
-    {
-        int64_t value = 2;
-        auto stmt = instance->createStatement("update tb_user set name = ? where id = ?;");
-        stmt->setText(1, "mike");
-        stmt->setLong(2, value);
-        auto count = stmt->executeUpdate();
-        printf("stmt update rows: %d\n", (int) count);
+    auto count = instance->executeUpdate(R"(update tb_update set name = "mike" where id = 1;)");
+    EXPECT_NE(-1, count) << "Update failure";
+
+    auto result1 = instance->executeQuery("select * from tb_update where id = 1;");
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
+        printf("result1 id = %d, name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+    }
+}
+
+// delete
+TEST(TestDriverInstance, TestDeleteData) {
+    auto instance = DriverManager::getInstance(DatabaseType::Sqlite, "/root/db_test.db");
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    auto result = instance->executeQuery("select * from tb_delete;");
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
+        printf("result id = %d, name = %s\n", (int) result->getInteger(0), result->getString(1).data());
     }
 
-    {
-        auto stmt = instance->createStatement("select * from tb_user;");
-        auto result = stmt->executeQuery();
-        while (result->next()) {
-            printf("stmt result: id = %d, name = %s\n", (int) result->getInteger(0), result->getString(1).data());
-        }
+    auto count = instance->executeUpdate("delete from tb_delete where id = 1;");
+    EXPECT_NE(-1, count) << "Delete failure";
+
+    auto result1 = instance->executeQuery("select * from tb_delete;");
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
+        printf("result1 id = %d, name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+    }
+}
+
+// insert
+TEST(TestDriverInstance, TestInsertData) {
+    auto instance = DriverManager::getInstance(DatabaseType::Sqlite, "/root/db_test.db");
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    auto result = instance->executeQuery("select * from tb_delete;");
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
+        printf("result id = %d, name = %s\n", (int) result->getInteger(0), result->getString(1).data());
     }
 
-    return 0;
+    auto count = instance->executeUpdate("insert into tb_insert (id, name) values (3, 'mike');");
+    EXPECT_NE(-1, count) << "Insertion failure";
+
+    auto result1 = instance->executeQuery("select * from tb_delete;");
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
+        printf("result1 id = %d, name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+    }
+}
+
+// create update_stmt
+TEST(TestDriverInstance, TestUpdatestmt) {
+    auto instance = DriverManager::getInstance(DatabaseType::Sqlite, "/root/db_test.db");
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    auto result = instance->executeQuery("select * from tb_stmt_update where id = 1;");
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
+        printf("result id = %d, name = %s\n", (int) result->getInteger(0), result->getString(0).data());
+    }
+
+    int64_t id = 1;
+    const char *name = "mike";
+    auto stmt = instance->createStatement("update tb_stmt_update set name = ? where id = ?;");
+    ASSERT_NE(nullptr, stmt) << "Failed to create update a preprocessed statement";
+
+    EXPECT_EQ(true, stmt->setText(1, name)) << "Failed to fill in the text value parameter";
+    EXPECT_EQ(true, stmt->setLong(2, id)) << "Failed to fill in the Long value parameter";
+
+    auto count = stmt->executeUpdate();
+    EXPECT_NE(-1, count) << "Failed to use the updated prepared statement";
+
+    auto result1 = instance->executeQuery("select * from tb_stmt_update where id = 1;");
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
+        printf("result1 id = %d, name = %s\n", (int) result1->getInteger(0), result1->getString(0).data());
+    }
+}
+
+// create select_stmt
+TEST(TestDriverInstance, TestSelectstmt) {
+    auto instance = DriverManager::getInstance(DatabaseType::Sqlite, "/root/db_test.db");
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    int64_t id = 1;
+    auto stmt = instance->createStatement("select * from tb_stmt_select where id = ?;");
+    ASSERT_NE(nullptr, stmt) << "Failed to create a query preprocessing statement";
+
+    EXPECT_EQ(true, stmt->setLong(1, id)) << "Failed to fill in the Long value parameter";
+
+    auto result = stmt->executeQuery();
+    EXPECT_NE(nullptr, result) << "Failed to use the query prepared statement";
+    while (result->next()) {
+        printf("stmt result: id = %d, name = %s\n", (int) result->getInteger(0), result->getString(1).data());
+    }
+}
+
+// create delete_stmt
+TEST(TestDriverInstance, TestDeletestmt) {
+    auto instance = DriverManager::getInstance(DatabaseType::Sqlite, "/root/db_test.db");
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    auto result = instance->executeQuery("select * from tb_stmt_delete;");
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
+        printf("stmt result: id = %d, name = %s\n", (int) result->getInteger(0), result->getString(1).data());
+    }
+
+    int64_t id = 1;
+    auto stmt = instance->createStatement("delete * from tb_stmt_delete where id = ?;");
+    ASSERT_NE(nullptr, stmt) << "Failed to create delete a preprocessed statement";
+
+    EXPECT_EQ(true, stmt->setLong(1, id)) << "Failed to fill in the Long value parameter";
+
+    auto count = stmt->executeUpdate();
+    EXPECT_NE(-1, count) << "Failed to use the delete prepared statement";
+
+    auto result1 = instance->executeQuery("select * from tb_stmt_delete;");
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
+        printf("stmt result1: id = %d, name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+    }
+}
+
+// create insert_stmt
+TEST(TestDriverInstance, TestInsertstmt) {
+    auto instance = DriverManager::getInstance(DatabaseType::Sqlite, "/root/db_test.db");
+    ASSERT_NE(nullptr, instance) << "Failed to create the database object instance";
+
+    auto result = instance->executeQuery("select * from tb_stmt_insert;");
+    EXPECT_NE(nullptr, result) << "Query failure";
+    while (result->next()) {
+        printf("stmt result: id = %d, name = %s\n", (int) result->getInteger(0), result->getString(1).data());
+    }
+
+    int64_t id = 3;
+    const char *name = "mike";
+    auto stmt = instance->createStatement("insert into tb_stmt_insert (id, name) values (?, ?);");
+    ASSERT_NE(nullptr, stmt) << "Failed to create insert a preprocessed statement";
+
+    EXPECT_EQ(true, stmt->setLong(1, id)) << "Failed to fill in the Long value parameter";
+    EXPECT_EQ(true, stmt->setText(2, name)) << "Failed to fill in the Test value parameter";
+
+    auto count = stmt->executeUpdate();
+    EXPECT_NE(-1, count) << "Failed to use the insert prepared statement";
+
+    auto result1 = instance->executeQuery("select * from tb_stmt_insert;");
+    EXPECT_NE(nullptr, result1) << "Query failure";
+    while (result1->next()) {
+        printf("stmt result1: id = %d, name = %s\n", (int) result1->getInteger(0), result1->getString(1).data());
+    }
 }
