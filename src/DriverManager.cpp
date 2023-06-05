0a9a861dc86b6457eb739fbd5b0b473bd6d84031
Kaoru <SHIINA_KAORU@outlook.com>
2023-06-05 23:28:59 +0800

    fix: 修复实例创建时可能产生的内存泄漏

--- src/DriverManager.cpp
+++ src/DriverManager.cpp
@@ -51,6 +51,7 @@ DriverInstance::Ptr DriverManager::getInstance(sese::db::DatabaseType type, cons
 
             if (!status) {
                 // 连接数据库失败
+                mysql_close(conn);
                 return nullptr;
             }
 
@@ -61,7 +62,12 @@ DriverInstance::Ptr DriverManager::getInstance(sese::db::DatabaseType type, cons
         case DatabaseType::Sqlite: {
             sqlite3 *conn;
             int rt = sqlite3_open_v2(connectionString, &conn, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, nullptr);
-            if (rt != 0) return nullptr;
+            if (rt != 0) {
+                if (conn) {
+                    sqlite3_close_v2(conn);
+                }
+                return nullptr;
+            }
             return std::make_unique<impl::SqliteDriverInstanceImpl>(conn);
         }
 #endif
