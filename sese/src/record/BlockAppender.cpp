2de8119bbc1ea23b1076dfc33a9106177e343632
Kaoru <SHIINA_KAORU@outlook.com>
2023-06-14 09:59:13 +0800

    fix: 为 BlockAppender 设定 max size 下限

--- sese/src/record/BlockAppender.cpp
+++ sese/src/record/BlockAppender.cpp
@@ -1,6 +1,8 @@
-#include <sese/record/BlockAppender.h>
+#include "sese/record/BlockAppender.h"
 #include "sese/text/DateTimeFormatter.h"
-#include <sese/thread/Locker.h>
+#include "sese/thread/Locker.h"
+
+#include <algorithm>
 
 using namespace sese;
 using namespace sese::record;
@@ -12,9 +14,10 @@ inline std::string getDateTimeString() {
 
 BlockAppender::BlockAppender(size_t blockMaxSize, sese::record::Level level)
     : AbstractAppender(level), maxSize(blockMaxSize) {
-#ifndef _DEBUG
-    maxSize = blockMaxSize < 1000 * 1024 ? 1000 * 1024 : blockMaxSize;
-#endif
+    // #ifndef _DEBUG
+    //     maxSize = blockMaxSize < 1000 * 1024 ? 1000 * 1024 : blockMaxSize;
+    // #endif
+    maxSize = std::min<decltype(maxSize)>(1024, maxSize);
     auto fileName = getDateTimeString();
     fileStream = FileStream::create(fileName, TEXT_WRITE_CREATE_TRUNC);
     bufferedStream = std::make_unique<BufferedStream>(fileStream, 4 * 1024);
