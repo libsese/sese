557c2c32b5afcc4bfc016b9361e03e881e2260eb
Kaoru <SHIINA_KAORU@outlook.com>
2023-06-26 12:01:25 +0800

    fix: 修复分词层级错误的问题

--- sese/src/config/yaml/YamlUtil.cpp
+++ sese/src/config/yaml/YamlUtil.cpp
@@ -1,6 +1,7 @@
 #include "sese/config/yaml/YamlUtil.h"
 
 #include <sstream>
+#include <string>
 #include <vector>
 
 int sese::yaml::YamlUtil::getSpaceCount(const std::string &line) noexcept {
@@ -43,75 +44,75 @@ ret:
 std::vector<std::string> sese::yaml::YamlUtil::tokenizer(const std::string &line) noexcept {
     std::vector<std::string> vector;
     std::stringstream stream;
-    bool checkSpace = false;// 检查多余空格
-    bool quot1 = false;     // 单引号 - 不转义
-    bool quot2 = false;     // 双引号 - 转义
-    bool tran = false;      // 是否转义
+    bool isStr = false;// 是否是字符串
+    bool quot1 = false;// 单引号 - 不转义
+    bool quot2 = false;// 双引号 - 转义
+    bool tran = false; // 是否转义
     for (decltype(auto) ch: line) {
-        if (tran) {
-            switch (ch) {
-                case 'n':
-                    stream << '\n';
-                    break;
-                case 'r':
-                    stream << '\r';
-                    break;
-                case '\"':
-                case '\'':
-                case '\\':
-                    stream << ch;
-                    break;
-                default:
-                    stream << '\0';
-                    break;
-            }
-            tran = false;
-        } else if (ch == '-') {
-            vector.emplace_back("-");
-            checkSpace = true;
-        } else if (ch == ':') {
-            auto str = stream.str();
-            if (!str.empty()) {
-                vector.emplace_back(stream.str());
-            }
-            vector.emplace_back(":");
-            checkSpace = true;
-            stream.str("");
-        } else if (ch == '\'') {
-            if (quot1) {
+        if (quot1) {
+            if (ch == '\'') {
+                isStr = false;
                 quot1 = false;
                 vector.emplace_back(stream.str());
                 stream.str("");
             } else {
-                quot1 = true;
-                checkSpace = false;
-            }
-        } else if (ch == '|') {
-            vector.emplace_back("|");
-        } else if (ch == '\"') {
-            if (quot1) {
                 stream << ch;
-            } else if (quot2) {
+            }
+        } else if (quot2) {
+            if (tran) {
+                switch (ch) {
+                    case 'n':
+                        stream << '\n';
+                        break;
+                    case 'r':
+                        stream << '\r';
+                        break;
+                    case '"':
+                    case '\'':
+                    case '\\':
+                        stream << ch;
+                        break;
+                    default:
+                        stream << '\0';
+                        break;
+                }
+                tran = false;
+            } else if (ch == '\"') {
+                isStr = false;
                 quot2 = false;
                 vector.emplace_back(stream.str());
                 stream.str("");
-            } else {
-                quot2 = true;
-                checkSpace = false;
-            }
-        } else if (ch == '\\') {
-            tran = true;
-        } else {
-            if (checkSpace) {
-                if (ch == ' ') {
-                    continue;
-                } else {
-                    checkSpace = false;
-                    stream << ch;
-                }
+            } else if (ch == '\\') {
+                tran = true;
             } else {
                 stream << ch;
             }
+        } else if (isStr) {
+            if (ch == ':') {
+                isStr = false;
+                vector.emplace_back(stream.str());
+                vector.emplace_back(":");
+                stream.str("");
+            } else {
+                stream << ch;
+            }
+        } else if (ch == '\'') {
+            quot1 = true;
+            isStr = true;
+        } else if (ch == '\"') {
+            quot2 = true;
+            isStr = true;
+        } else if (ch == ':') {
+            vector.emplace_back(":");
+        } else if (ch == '-') {
+            vector.emplace_back("-");
+        } else if (ch == '|') {
+            vector.emplace_back("|");
+        } else if (ch == ' ') {
+            continue;
+        } else {
+            stream << ch;
+            isStr = true;
         }
     }
 
