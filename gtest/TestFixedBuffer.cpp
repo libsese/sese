fe7e3cbe6083e0b75c18643c12500cbb22d977c5
kaoru <shiinasama2001@gmail.com>
2023-07-10 14:13:14 +0800

    test: 添加 FixedBuffer 测试

---
+++ gtest/TestFixedBuffer.cpp
@@ -0,0 +1,61 @@
+#include <sese/util/FixedBuffer.h>
+#include <sese/util/FixedBuilder.h>
+
+#include <gtest/gtest.h>
+
+TEST(TestFixedBuffer, Write) {
+    auto size = 16;
+    auto buffer = sese::FixedBuffer(size);
+
+    int64_t len;
+    std::string data0 = "1234567890";
+    len = buffer.write(data0.data(), data0.length());
+    ASSERT_EQ(len, data0.length());
+
+    std::string data1 = "ABCDEFGHIJK";
+    len = buffer.write(data1.c_str(), data1.length());
+    ASSERT_EQ(len, size - data0.length());
+}
+
+TEST(TestFixedBuffer, Read) {
+    auto size = 16;
+    auto buffer = sese::FixedBuffer(16);
+    ASSERT_EQ(buffer.write("ABCDEFGHIJKLMNOP", 16), 16);
+    
+    int64_t len;
+    char buf[9];
+    len = buffer.read(buf, 9);
+    ASSERT_EQ(len, 9);
+
+    len = buffer.read(buf, 9);
+    ASSERT_EQ(len, 7);
+}
+
+TEST(TestFixedBuffer, Peek) {
+    auto size = 16;
+    auto buffer = sese::FixedBuffer(16);
+    ASSERT_EQ(buffer.write("ABCDEFGHIJKLMNOP", 16), 16);
+    
+    int64_t len;
+    char buf[9];
+    len = buffer.peek(buf, 9);
+    ASSERT_EQ(len, 9);
+    buffer.trunc(len);
+
+    len = buffer.peek(buf, 9);
+    ASSERT_EQ(len, 7);
+    ASSERT_EQ(buffer.trunc(9), 7);
+}
+
+TEST(TestFixedBuffer, Misc) {
+    auto buffer = sese::FixedBuilder(10);
+    buffer.write("Hello", 5);
+
+    auto i = buffer;
+    ASSERT_EQ(i.getWriteableSize(), buffer.getWriteableSize());
+
+    auto k = std::move(buffer);
+    ASSERT_EQ(buffer.getWriteableSize(), 0);
+    k.reset();
+    ASSERT_EQ(k.getWriteableSize(), 10);
+}
