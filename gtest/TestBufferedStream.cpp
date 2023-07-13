a4ebccb300c1f24139e3f2c35b4cdae7d5b3969e
kaoru <shiinasama2001@gmail.com>
2023-07-13 15:30:51 +0800

    test: BufferedStream 单测和修复

--- gtest/TestBufferedStream.cpp
+++ gtest/TestBufferedStream.cpp
@@ -54,3 +54,47 @@ TEST(TestBufferedStream, TestBufferedStream_1) {
     buffered.clear();
     buffered.reset(bytes1);
 }
+
+TEST(TestBufferedStream, Output) {
+    using sese::ByteBuilder;
+    using sese::BufferedOutputStream;
+
+    auto bytes = std::make_shared<ByteBuilder>(1024);
+    auto buffered = BufferedOutputStream(bytes, 16);
+    ASSERT_EQ(buffered.getCap(), 16);
+    buffered.write("Hello, World", 12);
+    ASSERT_EQ(buffered.getLen(), 12);
+    ASSERT_EQ(bytes->getLength(), 0);
+    buffered.write("World", 5);
+    buffered.write("HelloHelloHelloHello", 20);
+    ASSERT_EQ(buffered.getLen(), 0);
+    ASSERT_EQ(bytes->getLength(), 37);
+}
+
+TEST(TestBufferedStream, Input) {
+    using sese::ByteBuilder;
+    using sese::BufferedInputStream;
+
+    auto bytes = std::make_shared<ByteBuilder>(1024);
+    bytes->write("Hello, World, Hello, World, Hello, World", 40);
+    auto buffered = BufferedInputStream(bytes, 8);
+
+    char buffer[1024]{};
+
+    buffered.read(buffer, 1);
+    ASSERT_EQ(buffered.getLen(), 8);
+    ASSERT_EQ(buffered.getPos(), 1);
+    ASSERT_EQ(buffer[0], 'H');
+
+    buffered.read(buffer, 7);
+    ASSERT_EQ(buffered.getLen(), 8);
+    ASSERT_EQ(buffered.getPos(), 8);
+    ASSERT_EQ(std::string_view(buffer), std::string_view("ello, W"));
+
+    buffered.read(buffer, 30);
+    ASSERT_EQ(std::string_view(buffer), std::string_view("orld, Hello, World, Hello, Wor"));
+
+    buffered.read(buffer, 3);
+    buffer[2] = 0;
+    ASSERT_EQ(std::string_view(buffer), std::string_view("ld"));
+}
