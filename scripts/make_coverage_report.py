827e1c88cabd926d297bc09563e6bb219cf84d62
kaoru <shiinasama2001@gmail.com>
2023-07-14 12:55:06 +0800

    build: 测试覆盖率报告生成脚本

--- scripts/make_coverage_report.py
+++ scripts/make_coverage_report.py
@@ -9,14 +9,18 @@ def main():
 
     build_dir = sys.argv[1]
 
-    if not os.path.exists(build_dir):
+    if not os.path.exists('{}/{}'.format(os.getcwd(), build_dir)):
         print('指定路径不存在')
         sys.exit(-1)
 
-    if not os.path.exists(os.getcwd() + '/coverage'):
-        os.mkdir('coverage')
+    cov_dir = '{}/coverage'.format(os.getcwd());
+    print('checking directory... {}'.format(cov_dir))
+    if not os.path.exists(cov_dir):
+        print('mkdir {}'.format(cov_dir))
+        os.mkdir(cov_dir)
 
-    sys.exit(os.system('gcovr -b -r {} --html --html-details -o coverage/index.html'.format(build_dir)))
+    print('process is running... gcov')
+    sys.exit(os.system('gcovr -b -r {} --html --html-details -o {}/index.html'.format(build_dir, cov_dir)))
 
 
 if __name__ == '__main__':
