d2e96944d346f494ed2f5616eb9b24bbff075d35
kaoru <shiina_kaoru@outlook.com>
2023-08-05 14:32:05 +0800

    feat: ThreadPool 支持获取返回值

--- sese/include/sese/thread/ThreadPool.h
+++ sese/include/sese/thread/ThreadPool.h
@@ -4,12 +4,16 @@
  * @date 2022年4月7日
  * @author kaoru
  */
+
 #pragma once
+
 #include "sese/Config.h"
 #include "sese/util/Noncopyable.h"
 #include "sese/thread/Thread.h"
+
 #include <atomic>
 #include <condition_variable>
+#include <future>
 #include <memory>
 #include <queue>
 #include <string>
@@ -20,7 +24,7 @@ namespace sese {
     /**
      * @brief 线程池类
      */
-    class API ThreadPool : Noncopyable {
+    class API ThreadPool : public Noncopyable {
     public:
         using Ptr = std::unique_ptr<ThreadPool>;
 
@@ -37,12 +41,23 @@ namespace sese {
          * 向线程池添加单个任务
          * @param task 欲执行的任务
          */
-        void postTask(const std::function<void ()> &task);
+        void postTask(const std::function<void()> &task);
+
         /**
          * 向线程池批量添加任务
          * @param tasks 欲执行的任务集合
          */
-        void postTask(const std::vector<std::function<void ()>> &tasks);
+        void postTask(const std::vector<std::function<void()>> &tasks);
+
+        /**
+         * 向线程池添加有返回值的任务
+         * \tparam ReturnType 返回值类型
+         * \param tasks 欲执行的任务
+         * \return std::packaged_task 对象
+         */
+        template<class ReturnType>
+        std::packaged_task<ReturnType()> postTask(const std::function<ReturnType()> &tasks);
+
         /**
          * @brief 关闭当前线程池并阻塞至子线程退出
          */
@@ -61,10 +76,17 @@ namespace sese {
         struct RuntimeData {
             std::mutex mutex;
             std::condition_variable conditionVariable;
-            std::queue<std::function<void ()>> tasks;
+            std::queue<std::function<void()>> tasks;
             std::atomic<bool> isShutdown{false};
         };
         std::shared_ptr<RuntimeData> data;
     };
 
 }// namespace sese
+
+template<class ReturnType>
+std::packaged_task<ReturnType()> sese::ThreadPool::postTask(const std::function<ReturnType()> &task) {
+    std::packaged_task<ReturnType()> packagedTask(task);
+    this->postTask([&]() { packagedTask(); });
+    return packagedTask;
+}
