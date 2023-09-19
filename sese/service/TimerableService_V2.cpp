ad4c0efdadaf3bb8516fbe46351ab51396d9578f
kaoru <shiinasama2001@gmail.com>
2023-09-19 21:39:40 +0800

    feat: TimerableService_V2 初步实现

---
+++ sese/service/TimerableService_V2.cpp
@@ -0,0 +1,30 @@
+#include <sese/service/TimerableService_V2.h>
+
+using namespace sese::event;
+using namespace sese::service;
+
+void TimerableService_V2::dispatch(uint32_t timeout) {
+    timeWheel.check();
+    EventLoop::dispatch(timeout);
+}
+
+void TimerableService_V2::onTimeout(TimeoutEvent_V2 *event) {
+}
+
+TimeoutEvent_V2 *TimerableService_V2::setTimeoutEvent(int64_t seconds, void *data) {
+    auto event = new TimeoutEvent_V2;
+    event->event = timeWheel.delay(
+            [&]() {
+                this->onTimeout(event);
+            },
+            seconds,
+            false
+    );
+    event->data = data;
+    return event;
+}
+
+void TimerableService_V2::cancelTimeoutEvent(sese::service::TimeoutEvent_V2 *event) {
+    timeWheel.cancel(event->event);
+    delete event;
+}
---
+++ sese/service/TimerableService_V2.h
@@ -0,0 +1,51 @@
+/// \file TimerableService_V2.h
+/// \author kaoru
+/// \date 2023年9月19日
+/// \brief 可定时服务
+/// \version 0.2.0
+
+#pragma once
+
+#include <sese/event/Event.h>
+#include <sese/util/TimeWheel.h>
+
+#if defined(_MSC_VER)
+#pragma warning(disable : 4275)
+#endif
+
+namespace sese::service {
+
+    /// 超时事件结构
+    struct TimeoutEvent_V2 {
+        TimeoutEvent *event{nullptr};
+        /// 额外数据
+        void *data{nullptr};
+    };
+
+    /// 可定时服务
+    class API TimerableService_V2 : public event::EventLoop {
+    public:
+        /// 分发事件
+        /// \param timeout 超时时间
+        void dispatch(uint32_t timeout) override;
+
+        /// 超时回调函数
+        /// \param event 事件
+        virtual void onTimeout(TimeoutEvent_V2 *event);
+
+        /// 设置超时事件
+        /// \param seconds 超时事件
+        /// \param data 附加数据
+        /// \return 超时事件结构
+        TimeoutEvent_V2 *setTimeoutEvent(int64_t seconds, void *data);
+
+        /// 取消并释放超时事件
+        /// \param event 超时事件
+        void cancelTimeoutEvent(TimeoutEvent_V2 *event);
+
+    protected:
+        /// 时间轮
+        TimeWheel timeWheel{};
+    };
+
+}// namespace sese::service
