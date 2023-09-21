7a66fb12c196044522876d84f7a04dddab30028c
kaoru <shiinasama2001@gmail.com>
2023-09-21 15:54:31 +0800

    feat: 完成基于 sese-event 的 IOCP Server

--- sese/service/iocp/IOCPServer_V1.cpp
+++ sese/service/iocp/IOCPServer_V1.cpp
@@ -10,16 +10,49 @@ using namespace sese::service;
 IOCPService_V1::IOCPService_V1(IOCPServer_V1 *master)
     : TimerableService_V2() {
     IOCPService_V1::master = master;
+
+    if (master->getServCtx()) {
+        auto serverSSL = (SSL_CTX *) master->getServCtx()->getContext();
+        SSL_CTX_set_alpn_select_cb(
+                serverSSL,
+                (SSL_CTX_alpn_select_cb_func) &alpnCallbackFunction,
+                this
+        );
+    }
+}
+
+IOCPService_V1::~IOCPService_V1() {
+    for (auto &&item :eventSet) {
+        event::EventLoop::freeEvent(item);
+    }
 }
 
 void IOCPService_V1::postRead(IOCPService_V1::Context *ctx) {
-    ctx->event->events |= EVENT_READ;
-    IOCPService_V1::setEvent(ctx->event);
+    if (ctx->event) {
+        ctx->event->events |= EVENT_READ;
+        IOCPService_V1::setEvent(ctx->event);
+    } else {
+        ctx->event = createEventEx(ctx->fd, EVENT_READ, ctx);
+        ctx->event->data = ctx;
+    }
 }
 
 void IOCPService_V1::postWrite(IOCPService_V1::Context *ctx) {
-    ctx->event->events |= EVENT_WRITE;
-    IOCPService_V1::setEvent(ctx->event);
+    if (ctx->event) {
+        ctx->event->events |= EVENT_WRITE;
+        IOCPService_V1::setEvent(ctx->event);
+    } else {
+        ctx->event = createEventEx(ctx->fd, EVENT_WRITE, ctx);
+        ctx->event->data = ctx;
+    }
+}
+
+void IOCPService_V1::onAcceptCompleted(IOCPService_V1::Context *ctx) {
+    ctx->self->onAcceptCompleted(ctx);
+}
+
+void IOCPService_V1::onPreRead(IOCPService_V1::Context *ctx) {
+    ctx->self->onPreRead(ctx);
 }
 
 void IOCPService_V1::onReadCompleted(IOCPService_V1::Context *ctx) {
@@ -34,14 +67,109 @@ void IOCPService_V1::onTimeout(IOCPService_V1::Context *ctx) {
     ctx->self->onTimeout(ctx);
 }
 
-// todo 实现 IOCP 核心功能
+void IOCPService_V1::onAlpnGet(IOCPService_V1::Context *ctx, const uint8_t *in, uint32_t inLength) {
+    ctx->self->onAlpnGet(ctx, in, inLength);
+}
+
 void IOCPService_V1::onAccept(int fd) {
+    SSL *clientSSL;
+    if (sese::net::Socket::setNonblocking(fd)) {
+        sese::net::Socket::close(fd);
+    }
+
+    auto ctx = new Context;
+    ctx->self = master;
+    ctx->client = this;
+    ctx->fd = fd;
+
+    if (master->getServCtx()) {
+        auto serverSSL = (SSL_CTX *) master->getServCtx()->getContext();
+        clientSSL = SSL_new(serverSSL);
+        SSL_set_fd(clientSSL, fd);
+        SSL_set_accept_state(clientSSL);
+
+        while (true) {
+            auto rt = SSL_do_handshake(clientSSL);
+            if (rt <= 0) {
+                auto err = SSL_get_error(clientSSL, rt);
+                if (err != SSL_ERROR_WANT_READ && err != SSL_ERROR_WANT_WRITE) {
+                    SSL_free(clientSSL);
+                    sese::net::Socket::close(fd);
+                    delete ctx;
+                    return;
+                }
+            } else {
+                ctx->ssl = clientSSL;
+                SSL_set_mode(clientSSL, SSL_MODE_ACCEPT_MOVING_WRITE_BUFFER);
+                const uint8_t *data = nullptr;
+                uint32_t dataLength;
+                SSL_get0_alpn_selected(clientSSL, &data, &dataLength);
+                onAlpnGet(ctx, data, dataLength);
+                break;
+            }
+        }
+    }
+
+    onAcceptCompleted(ctx);
 }
 
 void IOCPService_V1::onRead(sese::event::BaseEvent *event) {
+    auto ctx = (Context *) event->data;
+
+    onPreRead(ctx);
+
+    char buf[MTU_VALUE];
+    while (true) {
+        auto l = read(ctx->fd, buf, MTU_VALUE, ctx->ssl);
+        if (l <= 0) {
+            auto err = sese::net::getNetworkError();
+            if (err == ENOTCONN) {
+                ctx->self->getDeleteContextCallback()(ctx);
+                if (ctx->ssl) {
+                    SSL_free((SSL *) ctx->ssl);
+                }
+                sese::net::Socket::close(ctx->fd);
+                freeEventEx(ctx->event);
+                break;
+            } else {
+                onReadCompleted(ctx);
+                break;
+            }
+        } else {
+            ctx->recv.write(buf, l);
+        }
+    }
 }
 
 void IOCPService_V1::onWrite(sese::event::BaseEvent *event) {
+    auto ctx = (Context *) event->data;
+    char buf[MTU_VALUE];
+    while (true) {
+        auto len = ctx->send.peek(buf, MTU_VALUE);
+        if (len == 0) {
+            ctx->send.freeCapacity();
+            onWriteCompleted(ctx);
+            break;
+        }
+        auto l = write(ctx->fd, buf, len, ctx->ssl);
+        if (l <= 0) {
+            auto err = sese::net::getNetworkError();
+            if (err == EWOULDBLOCK || err == EINTR) {
+                postWrite(ctx);
+                break;
+            } else {
+                ctx->self->getDeleteContextCallback()(ctx);
+                if (ctx->ssl) {
+                    SSL_free((SSL *) ctx->ssl);
+                }
+                sese::net::Socket::close(ctx->fd);
+                freeEventEx(ctx->event);
+                break;
+            }
+        } else {
+            ctx->send.trunc(l);
+        }
+    }
 }
 
 void IOCPService_V1::onTimeout(TimeoutEvent_V2 *event) {
@@ -53,9 +181,51 @@ int IOCPService_V1::alpnCallbackFunction([[maybe_unused]] void *ssl, const uint8
     return service->master->onAlpnSelect(out, outLength, in, inLength);
 }
 
+sese::event::BaseEvent *IOCPService_V1::createEventEx(int fd, unsigned int events, void *data) {
+    auto event = createEvent(fd, events, data);
+    eventSet.emplace(event);
+    return event;
+}
+
+void IOCPService_V1::freeEventEx(sese::event::BaseEvent *event) {
+    eventSet.erase(event);
+    freeEvent(event);
+}
+
+int64_t IOCPService_V1::read(int fd, void *buffer, size_t len, void *ssl) {
+    if (ssl) {
+        return SSL_read((SSL *) ssl, buffer, (int) len);
+    } else {
+        return sese::net::Socket::read(fd, buffer, len, 0);
+    }
+}
+
+int64_t IOCPService_V1::write(int fd, const void *buffer, size_t len, void *ssl) {
+    if (ssl) {
+        return SSL_write((SSL *) ssl, buffer, (int) len);
+    } else {
+        return sese::net::Socket::write(fd, buffer, len, 0);
+    }
+}
+
 #pragma endregion Service
 
 #pragma region Server
+
+bool IOCPServer_V1::init() {
+    auto rt = balanceLoader.init<IOCPService_V1>([this]() -> auto {
+        return new IOCPService_V1(this);
+    });
+    if (rt) {
+        balanceLoader.start();
+        return true;
+    }
+    return false;
+}
+
+void IOCPServer_V1::shutdown() {
+    balanceLoader.stop();
+}
 
 void IOCPServer_V1::postRead(IOCPServer_V1::Context *ctx) {
     ctx->client->postRead(ctx);
--- sese/service/iocp/IOCPServer_V1.h
+++ sese/service/iocp/IOCPServer_V1.h
@@ -3,7 +3,10 @@
 #include <sese/Config.h>
 #include <sese/service/BalanceLoader.h>
 #include <sese/service/TimerableService_V2.h>
+#include <sese/security/SSLContext.h>
 #include <sese/io/ByteBuilder.h>
+
+#include <set>
 
 namespace sese::iocp {
 
@@ -14,28 +17,53 @@ namespace sese::iocp {
     class IOCPServer_V1 {
     public:
         using Context = Context_V1;
+        using DeleteContextCallback = std::function<void(Context *data)>;
 
+        bool init();
+        void shutdown();
         static void postRead(Context *ctx);
         static void postWrite(Context *ctx);
         static void setTimeout(Context *ctx, int64_t seconds);
         static void cancelTimeout(Context *ctx);
+        static void onDeleteContext(Context *) {}
+        virtual void onAcceptCompleted(Context *ctx){};
+        virtual void onPreRead(Context *ctx){}
         virtual void onReadCompleted(Context *ctx){};
         virtual void onWriteCompleted(Context *ctx){};
         virtual void onTimeout(Context *ctx){};
-        virtual void onAlpnGet(const uint8_t *in, uint32_t inLength){};
+        virtual void onAlpnGet(Context *ctx, const uint8_t *in, uint32_t inLength){};
         int onAlpnSelect(
                 const uint8_t **out, uint8_t *outLength,
                 const uint8_t *in, uint32_t inLength
         );
 
+    public:
+        void setAcceptTimeout(uint32_t seconds) { balanceLoader.setAcceptTimeout(seconds); }
+        void setDispatchTimeout(uint32_t seconds) { balanceLoader.setDispatchTimeout(seconds); }
+        void setAddress(const net::IPAddress::Ptr &addr) { balanceLoader.setAddress(addr); }
+        void setKeepalive(uint32_t seconds) { IOCPServer_V1::keepalive = seconds; }
+        void setThreads(size_t threads) { balanceLoader.setThreads(threads); }
+        void setServCtx(const security::SSLContext::Ptr &ctx) { IOCPServer_V1::sslCtx = ctx; }
+        void setServProtos(const std::string &protos) { IOCPServer_V1::servProtos = protos; }
+        void setDeleteContextCallback(const DeleteContextCallback &callback) { IOCPServer_V1::deleteContextCallback = callback; }
+
+        [[nodiscard]] uint32_t getKeepalive() const { return IOCPServer_V1::keepalive; }
+        [[nodiscard]] const security::SSLContext::Ptr &getServCtx() const { return IOCPServer_V1::sslCtx; }
+        [[nodiscard]] const DeleteContextCallback &getDeleteContextCallback() const { return IOCPServer_V1::deleteContextCallback; };
+
     protected:
-        std::string servProtos {};
+        uint32_t keepalive{};
+        DeleteContextCallback deleteContextCallback = onDeleteContext;
+        security::SSLContext::Ptr sslCtx{};
+        std::string servProtos{};
+        service::BalanceLoader balanceLoader;
     };
 
     struct Context_V1 {
         IOCPServer_V1 *self{};
         IOCPService_V1 *client{};
         socket_t fd{0};
+        void *ssl{};
         event::BaseEvent *event{};
         service::TimeoutEvent_V2 *timeoutEvent{};
         io::ByteBuilder send{8192, 8192};
@@ -48,12 +76,16 @@ namespace sese::iocp {
         using Context = Context_V1;
 
         explicit IOCPService_V1(IOCPServer_V1 *master);
+        ~IOCPService_V1() override;
 
         void postRead(Context *ctx);
         void postWrite(Context *ctx);
+        static void onAcceptCompleted(Context *ctx);
+        static void onPreRead(Context *ctx);
         static void onReadCompleted(Context *ctx);
         static void onWriteCompleted(Context *ctx);
         static void onTimeout(Context *ctx);
+        static void onAlpnGet(Context *ctx, const uint8_t *in, uint32_t inLength);
         static int alpnCallbackFunction(
                 void *ssl,
                 const uint8_t **out, uint8_t *outLength,
@@ -66,8 +98,15 @@ namespace sese::iocp {
         void onWrite(event::BaseEvent *event) override;
         void onTimeout(service::TimeoutEvent_V2 *event) override;
 
+        event::BaseEvent *createEventEx(int fd, unsigned int events, void *data);
+        void freeEventEx(sese::event::BaseEvent *event);
+
     private:
+        static int64_t read(int fd, void *buffer, size_t len, void *ssl);
+        static int64_t write(int fd, const void *buffer, size_t len, void *ssl);
+
         IOCPServer_V1 *master{};
+        std::set<event::BaseEvent *> eventSet;
     };
 
 }// namespace sese::iocp
