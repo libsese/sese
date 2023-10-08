56d1548efc3c1f16e0a3c3e9db2c6704ac0b465f
kaoru <shiinasama2001@gmail.com>
2023-10-08 19:15:54 +0800

    feat: File locker implementation on linux fix: Remove the useless function

---
+++ sese/native/Linux/system/FileLocker.cpp
@@ -0,0 +1,36 @@
+#include <sese/system/FileLocker.h>
+
+#include <fcntl.h>
+
+bool sese::system::FileLocker::lockWrite(int64_t start, int64_t len) {
+    struct flock lock {};
+    lock.l_type = F_WRLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = start;
+    lock.l_len = len;
+    return ::fcntl(fd, F_SETLKW, &lock) == 0;
+}
+
+bool sese::system::FileLocker::lockRead(int64_t start, int64_t len) {
+    struct flock lock {};
+    lock.l_type = F_RDLCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = start;
+    lock.l_len = len;
+    return ::fcntl(fd, F_SETLKW, &lock) == 0;
+}
+
+bool sese::system::FileLocker::lock(int64_t start, int64_t len) {
+    struct flock lock {};
+    lock.l_type = F_LOCK;
+    lock.l_whence = SEEK_SET;
+    lock.l_start = start;
+    lock.l_len = len;
+    return ::fcntl(fd, F_SETLK, &lock) == 0;
+}
+
+bool sese::system::FileLocker::unlock() {
+    struct flock lock {};
+    lock.l_type = F_UNLCK;
+    return ::fcntl(fd, F_SETLK, &lock) == 0;
+}
