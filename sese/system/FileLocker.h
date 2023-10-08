528a8e1b9144314796d42f3164d0594335f8ac03
kaoru <shiina_kaoru@outlook.com>
2023-10-09 00:04:59 +0800

    feat: File locker implementation on macos

--- sese/system/FileLocker.h
+++ sese/system/FileLocker.h
@@ -1,17 +1,26 @@
+/**
+ * @file FileLocker.cpp
+ * @brief kaoru
+ * @author 文件锁
+ */
+
 #pragma once
 
 #include <sese/Config.h>
 
 namespace sese::system {
 
+    /**
+     * 文件锁
+     */
     class API FileLocker {
     public:
         explicit FileLocker(int32_t fd) : fd(fd) {}
 
-        bool lockWrite(int64_t start, int64_t len);
-        bool lockRead(int64_t start, int64_t len);
-        bool lock(int64_t start, int64_t len);
-        bool unlock();
+        bool lockWrite(int64_t start, int64_t len) const;
+        bool lockRead(int64_t start, int64_t len) const;
+        bool lock(int64_t start, int64_t len) const;
+        bool unlock() const;
 
     private:
         int32_t fd{};
