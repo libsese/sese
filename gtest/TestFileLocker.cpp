4bd5543dd078d01997dcf79a0ce7e779e604b19b
kaoru <shiinasama2001@gmail.com>
2023-10-09 15:24:30 +0800

    test: file locker testing

---
+++ gtest/TestFileLocker.cpp
@@ -0,0 +1,65 @@
+#include <sese/system/FileLocker.h>
+#include <sese/io/File.h>
+#include <sese/thread/Async.h>
+
+#include <gtest/gtest.h>
+
+#include <filesystem>
+
+TEST(TestFileLocker, Lock_1) {
+    auto file = sese::io::File::create("temp.txt", BINARY_BOTH_CREATE_TRUNC);
+    ASSERT_NE(file, nullptr);
+
+    auto func = [file]() {
+        auto fd = file->getFd();
+        sese::system::FileLocker locker(fd);
+        locker.lockWrite(0, 1024);
+        file->write("Hello ", 6);
+        file->write("World\n", 6);
+        locker.unlock();
+        file->flush();
+    };
+
+    auto future = sese::async<void>(func);
+    future.get();
+    func();
+
+    char buffer[1024]{};
+    file->setSeek(0, SEEK_SET);
+    file->read(buffer, 1024);
+    EXPECT_EQ(std::string("Hello World\nHello World\n"), buffer);
+    file->close();
+
+    std::filesystem::remove("temp.txt");
+}
+
+TEST(TestFileLocker, Lock_2) {
+    auto file = sese::io::File::create("temp.txt", BINARY_BOTH_CREATE_TRUNC);
+    ASSERT_NE(file, nullptr);
+
+    auto func = [file]() {
+        auto fd = file->getFd();
+        sese::system::FileLocker locker(fd);
+        locker.lock(0, 1024);
+        file->write("Hello ", 6);
+        file->write("World\n", 6);
+        locker.unlock();
+        file->flush();
+    };
+
+    auto future = sese::async<void>(func);
+    future.get();
+    func();
+
+    auto fd = file->getFd();
+    sese::system::FileLocker locker(fd);
+    locker.lockRead(0, 1024);
+    char buffer[1024]{};
+    file->setSeek(0, SEEK_SET);
+    file->read(buffer, 1024);
+    locker.unlock();
+    EXPECT_EQ(std::string("Hello World\nHello World\n"), buffer);
+    file->close();
+
+    std::filesystem::remove("temp.txt");
+}
