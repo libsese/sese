9d26e567fb48899039571b5b92897721b8356cf7
kaoru <shiinasama2001@gmail.com>
2023-10-13 16:30:23 +0800

    refactor: Linked Queue

--- gtest/TestConcurrent.cpp
+++ gtest/TestConcurrent.cpp
@@ -1,4 +1,5 @@
 #include <sese/concurrent/LinkedStack.h>
+#include <sese/concurrent/LinkedQueue.h>
 #include <sese/thread/Async.h>
 #include <sese/record/Marco.h>
 
@@ -49,6 +50,76 @@ TEST(TestConcurrent, LinkedStack_PushAndPop) {
         int value;
         while (true) {
             if (stack.pop(value)) {
+                consume += 1;
+                // SESE_DEBUG("OK");
+            } else {
+                SESE_WARN("FAILED");
+            }
+            if (consume == expect) {
+                break;
+            }
+        }
+    };
+
+    auto p1 = sese::async<void>(produceProc);
+    auto p2 = sese::async<void>(produceProc);
+
+    auto c1 = sese::async<void>(consumeProc);
+    auto c2 = sese::async<void>(consumeProc);
+
+    p1.get();
+    c1.get();
+    p2.get();
+    c2.get();
+
+    EXPECT_EQ(produce, consume);
+}
+
+TEST(TestConcurrent, LinkedQueue_Push) {
+    using sese::concurrent::LinkedQueue;
+
+    LinkedQueue<int> queue;
+    auto proc = [&queue] {
+        for (int i = 0; i < 10000; ++i) {
+            queue.push(1);
+        }
+    };
+
+    auto f1 = sese::async<void>(proc);
+    auto f2 = sese::async<void>(proc);
+    auto f3 = sese::async<void>(proc);
+    auto f4 = sese::async<void>(proc);
+    f1.get();
+    f2.get();
+    f3.get();
+    f4.get();
+
+    int count = 0;
+    int value;
+    while (queue.pop(value)) {
+        count += 1;
+    }
+    EXPECT_EQ(count, 40000);
+}
+
+TEST(TestConcurrent, LinkedQueue_PushAndPop) {
+    using sese::concurrent::LinkedQueue;
+
+    std::atomic<int> produce = 0;
+    std::atomic<int> consume = 0;
+    const int expect = 20000;
+    LinkedQueue<int> queue;
+    auto produceProc = [&queue, &produce] {
+        for (int i = 0; i < 10000; ++i) {
+            produce += 1;
+            queue.push(1);
+            // SESE_DEBUG("PUSH");
+        }
+    };
+    auto consumeProc = [&queue, &consume] {
+        int value;
+        while (true) {
+            if (queue.pop(value)) {
                 consume += 1;
                 // SESE_DEBUG("OK");
             } else {
--- sese/concurrent/LinkedQueue.h
+++ sese/concurrent/LinkedQueue.h
@@ -3,142 +3,87 @@
  * @author kaoru
  * @date 2022年5月30日
  * @brief 非阻塞线程安全队列
- * @bug 队列长度超过10^3时可能产生意想不到的错误，暂时原因不明
- * @bug 已知可并发读、并发写，但无法做到并发读写，尚未有继续研究的计划
- * @see https://github.com/openjdk/jdk/blob/master/src/java.base/share/classes/java/util/concurrent/ConcurrentLinkedQueue.java
+ * @see https://dl.acm.org/doi/pdf/10.1145/248052.248106
  */
 #pragma once
-#include <sese/concurrent/CASDefine.h>
+
+#include <atomic>
 
 namespace sese::concurrent {
 
-/**
- * @brief 非阻塞线程安全队列
- * @tparam T 数据类型
- * @deprecated 弃用原因请查看文件文档
- */
-template<typename T>
-class SESE_DEPRECATED_WITH("已弃用") LinkedQueue {
+template<class T>
+class LinkedQueue {
+    struct Node {
+        std::atomic<T> value{};
+        std::atomic<Node *> next{nullptr};
+    };
+
+    std::atomic<Node *> head{nullptr};
+    std::atomic<Node *> tail{nullptr};
+
 public:
-    using NodeType = Node<T>;
-
-    virtual ~LinkedQueue() {
-        auto toDel = (NodeType *) head;
-        while (toDel != nullptr) {
-            auto next = toDel->next;
-            delete toDel;
-            toDel = next;
-        }
+    LinkedQueue() {
+        auto node = new Node();
+        head.store(node);
+        tail.store(node);
     }
 
-    [[nodiscard]] bool empty() const { return head == nullptr; }
-
-    [[nodiscard]] size_t size() const {
-        size_t size = 0;
-        auto p = (NodeType *) head;
-        while (p != nullptr) {
-            size++;
-            p = p->next;
-        }
-        return size;
-    }
-
-    void push(T &&value) {
-        auto newNode = new NodeType;
-        newNode->value = value;
-
-        if (head == nullptr) {
-            // 队列空
-            // 优先更新 head 节点
-            while (!compareAndSwapPointer((void *volatile *) &head, nullptr, newNode)) {}
-            compareAndSwapPointer((void *volatile *) &tail, nullptr, newNode);
-        } else if (tail == nullptr) {
-            // 可能 tail 没能更新成功导致仍为 nullptr
-            // 故从 head 开始查找尾部位置
-            // assert(head != nullptr)
-            auto realTail = findTail((NodeType *) head);
-            while (!compareAndSwapPointer((void *volatile *) &realTail, nullptr, newNode)) {}
-            compareAndSwapPointer((void *volatile *) &tail, nullptr, newNode);
-        } else {
-            // 正常插入，tail 不一定是尾部节点，但一定是最靠近尾部的
-            // 故从 tail 开始查找尾部位置
-            // assert(tail != nullptr)
-            auto realTail = findTail((NodeType *) tail);
-            while (!compareAndSwapPointer((void *volatile *) &realTail->next, nullptr, newNode)) {}
-            compareAndSwapPointer((void *volatile *) &tail, realTail, newNode);
+    ~LinkedQueue() {
+        auto pNode = head.load();
+        while (pNode) {
+            auto pNext = pNode->next.load();
+            if (pNext == pNode) {
+                pNext = nullptr;
+            }
+            delete pNode;
+            pNode = pNext;
         }
     }
 
     void push(const T &value) {
-        auto newNode = new NodeType;
-        newNode->value = value;
-
-        if (head == nullptr) {
-            // 队列空
-            // 优先更新 head 节点
-            while (!compareAndSwapPointer((void *volatile *) &head, nullptr, newNode)) {}
-            compareAndSwapPointer((void *volatile *) &tail, nullptr, newNode);
-        } else if (tail == nullptr) {
-            // 可能 tail 没能更新成功导致仍为 nullptr
-            // 故从 head 开始查找尾部位置
-            // assert(head != nullptr)
-            auto realTail = findTail((NodeType *) head);
-            while (!compareAndSwapPointer((void *volatile *) &realTail, nullptr, newNode)) {}
-            compareAndSwapPointer((void *volatile *) &tail, nullptr, newNode);
-        } else {
-            // 正常插入，tail 不一定是尾部节点，但一定是最靠近尾部的
-            // 故从 tail 开始查找尾部位置
-            // assert(tail != nullptr)
-            auto oldTail = (NodeType *) tail;
-            auto realTail = findTail((NodeType *) tail);
-            while (!compareAndSwapPointer((void *volatile *) &realTail->next, nullptr, newNode)) {}
-            compareAndSwapPointer((void *volatile *) &tail, (NodeType *) oldTail, newNode);
-        }
-    }
-
-    T pop(T &&defaultValue) {
-        if (head == nullptr) {
-            // 队列空，直接返回缺省值
-            return defaultValue;
-        } else {
-            NodeType *oldHead = (NodeType *) head;
-            NodeType *node = (NodeType *) head;
-            //bug: may be head was nullptr
-            while (!compareAndSwapPointer((void *volatile *) &head, node, node->next)) {
-                node = (NodeType *) head;
+        Node *tail;
+        auto node = new Node();
+        node->value.store(value);
+        node->next.store(nullptr);
+        while (true) {
+            tail = this->tail.load();
+            auto next = tail->next.load();
+            if (tail == this->tail) {
+                if (next == nullptr) {
+                    if (tail->next.compare_exchange_weak(next, node)) {
+                        break;
+                    }
+                } else {
+                    this->tail.compare_exchange_weak(tail, next);
+                }
             }
-            T res = oldHead->value;
-            delete oldHead;
-            return res;
         }
+        this->tail.compare_exchange_weak(tail, node);
     }
 
-    T pop(const T &defaultValue) {
-        if (head == nullptr) {
-            // 队列空，直接返回缺省值
-            return defaultValue;
-        } else {
-            NodeType *node = (NodeType *) head;
-            //bug: may be head was nullptr
-            while (!compareAndSwapPointer((void *volatile *) &head, node, node->next)) {
-                node = (NodeType *) head;
+    bool pop(T &value) {
+        Node *head;
+        while (true) {
+            head = this->head.load();
+            auto tail = this->tail.load();
+            auto next = head->next.load();
+            if (head == this->head) {
+                if (head == tail) {
+                    if (next == nullptr) {
+                        return false;
+                    }
+                    this->tail.compare_exchange_weak(tail, next);
+                } else {
+                    value = next->value;
+                    if (this->head.compare_exchange_weak(head, next)) {
+                        break;
+                    }
+                }
             }
-            return node->value;
         }
+        delete head;
+        return true;
     }
-
-private:
-    NodeType *findTail(NodeType *from) {
-        if (from == nullptr) {
-            return (NodeType *) head;
-        }
-        while (from->next != nullptr) {
-            from = from->next;
-        }
-        return from;
-    }
-
-    volatile NodeType *head = nullptr;
-    volatile NodeType *tail = nullptr;
 };
+
 } // namespace sese::concurrent
