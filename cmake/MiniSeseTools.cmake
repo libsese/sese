6780e178026acd85d9d714e18e50003559409935
Kaoru <shiinasama2001@gmail.com>
2024-02-18 01:58:50 +0800

    build: simplified configuration

---
+++ cmake/MiniSeseTools.cmake
@@ -0,0 +1,81 @@
+macro(sese_auto_enable_feature opt_name feature_name)
+    if (${opt_name})
+        if (NOT DEFINED VCPKG_MANIFEST_FEATURES)
+            message(STATUS "Auto append features: ${feature_name}")
+            set(VCPKG_MANIFEST_FEATURES ${feature_name})
+        else ()
+            list(FIND VCPKG_MANIFEST_FEATURES ${opt_name} index)
+            if (index EQUAL -1)
+                message(STATUS "Auto append features: ${feature_name}")
+                list(APPEND VCPKG_MANIFEST_FEATURES ${feature_name})
+            endif ()
+        endif ()
+    endif ()
+endmacro()
+
+macro (SESE_PROXY ADDR)
+    set($ENV{http_proxy} ${ADDR})
+    set($ENV{https_proxy} ${ADDR})
+    set($ENV{all_proxy} ${ADDR})
+endmacro ()
+
+macro (SESE_CLEAN_PROXY)
+    set($ENV{http_proxy} "")
+    set($ENV{https_proxy} "")
+    set($ENV{all_proxy} "")
+endmacro ()
+
+macro(SESE_AUTO_DETECT_TRIPLET)
+    if (WIN32)
+        set (VCPKG_TARGET_TRIPLET x64-windows)
+    elseif (UNIX AND NOT APPLE)
+        set (VCPKG_TARGET_TRIPLET x64-linux-dynamic)
+    elseif (APPLE)
+        set (VCPKG_TARGET_TRIPLET x64-osx-dynamic)
+    else ()
+        message (FATAL_ERROR "Unknown systems")
+    endif ()
+endmacro()
+
+macro(SESE_AUTO_FIND_VCPKG)
+    if (NOT VCPKG_FOUND)
+        if (DEFINED CMAKE_TOOLCHAIN_FILE)
+            get_filename_component(FILENAME ${CMAKE_TOOLCHAIN_FILE} NAME)
+            if (${FILENAME} STREQUAL "vcpkg.cmake")
+                set(VCPKG_FOUND ON)
+            endif ()
+        endif ()
+
+        if (NOT VCPKG_FOUND)
+            if (NOT $ENV{VCPKG_ROOT} STREQUAL "")
+                set(VCPKG_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
+                message(STATUS "VCPKG_TOOLCHAIN_FILE :${VCPKG_TOOLCHAIN_FILE}")
+                include(${VCPKG_TOOLCHAIN_FILE})
+            else ()
+                find_path(VCPKG_TOOLCHAIN_DIR vcpkg.cmake
+                        C:/src/vcpkg/scripts/buildsystems
+                        C:/vcpkg/scripts/buildsystems
+                        D:/vcpkg/scripts/buildsystems
+                        E:/vcpkg/scripts/buildsystems
+                        F:/vcpkg/scripts/buildsystems
+                        C:/Users/$ENV{USER}/vcpkg/scripts/buildsystems
+                        C:/Users/$ENV{USER}/.vcpkg/scripts/buildsystems
+                        /usr/local/vcpkg/scripts/buildsystems
+                        /usr/local/share/vcpkg/scripts/buildsystems
+                        /src/vcpkg/scripts/buildsystems
+                        /home/$ENV{USER}/vcpkg/scripts/buildsystems
+                        /home/$ENV{USER}/.vcpkg/scripts/buildsystems
+                        /Users/$ENV{USER}/vcpkg/scripts/buildsystems
+                        /Users/$ENV{USER}/.vcpkg/scripts/buildsystems
+                )
+
+                if (${VCPKG_TOOLCHAIN_DIR} STREQUAL "VCPKG_TOOLCHAIN_DIR-NOTFOUND")
+                    message(FATAL_ERROR "Could not found the vcpkg.cmake")
+                else ()
+                    message(STATUS "VCPKG_TOOLCHAIN_FILE :${VCPKG_TOOLCHAIN_DIR}/vcpkg.cmake")
+                    include("${VCPKG_TOOLCHAIN_DIR}/vcpkg.cmake")
+                endif ()
+            endif ()
+        endif ()
+    endif ()
+endmacro()
