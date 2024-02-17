48e15e06cd5226da0b82b62130b7756358a95c02
kaoru <shiina_kaoru@outlook.com>
2024-02-17 21:12:08 +0800

    feat: RingQueue

---
+++ gtest/TestRingQueue.cpp
@@ -0,0 +1,125 @@
+#include <gtest/gtest.h>
+#include <sese/thread/Thread.h>
+#include <sese/container/BufferedQueue.h>
+#include <sese/record/Marco.h>
+#include <sese/util/StopWatch.h>
+#include <sese/container/RingQueue.h>
+
+using sese::RingQueue;
+
+TEST(TestRingQueue, PushAndPop) {
+    RingQueue<int, 3> queue;
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    ASSERT_TRUE(queue.full());
+    ASSERT_EQ(queue.size(), 3);
+    ASSERT_EQ(queue.front(), 1);
+    queue.pop();
+    ASSERT_EQ(queue.front(), 2);
+    ASSERT_FALSE(queue.empty());
+    queue.push(4);
+}
+
+TEST(TestRingQueue, Empty) {
+    RingQueue<int, 3> queue;
+    ASSERT_TRUE(queue.empty());
+    queue.push(1);
+    ASSERT_FALSE(queue.empty());
+}
+
+TEST(TestRingQueue, Full) {
+    RingQueue<int, 3> queue;
+    ASSERT_FALSE(queue.full());
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    ASSERT_TRUE(queue.full());
+}
+
+TEST(TestRingQueue, Pop) {
+    RingQueue<int, 3> queue;
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    ASSERT_EQ(queue.front(), 1);
+    queue.pop();
+    ASSERT_EQ(queue.front(), 2);
+    queue.pop();
+    ASSERT_EQ(queue.front(), 3);
+    queue.pop();
+    ASSERT_TRUE(queue.empty());
+    ASSERT_THROW(queue.front(), std::out_of_range);
+    ASSERT_THROW(queue.pop(), std::out_of_range);
+}
+
+TEST(TestRingQueue, Size) {
+    RingQueue<int, 3> queue;
+    ASSERT_EQ(queue.size(), 0);
+    queue.push(1);
+    queue.push(2);
+    ASSERT_EQ(queue.size(), 2);
+}
+
+TEST(TestRingQueue, Push) {
+    RingQueue<int, 3> queue;
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    ASSERT_THROW(queue.push(1), std::out_of_range);
+}
+
+TEST(TestRingQueue, MutilThread) {
+    const auto total = 4000;
+    RingQueue<int, 500> queue;
+
+    std::mutex mutex1;
+    auto proc1 = [&] {
+        sese::StopWatch watch;
+        for (int i = 0; i < total / 2;) {
+            mutex1.lock();
+            if (!queue.full()) {
+                queue.push(i);
+                i += 1;
+            } else  {
+                SESE_DEBUG("full hit");
+            }
+            mutex1.unlock();
+        }
+        auto time = watch.stop();
+        SESE_INFO("time cost: %zu ms", static_cast<size_t>(time.getTotalMilliseconds()));
+    };
+    sese::Thread thread1(proc1, "producer1");
+    sese::Thread thread2(proc1, "producer2");
+
+    std::mutex mutex2;
+    auto times = 0;
+    auto proc2 = [&] {
+        sese::StopWatch watch;
+        for (int i = 0; i < total / 2;) {
+            mutex2.lock();
+            if (!queue.empty()) {
+                queue.pop();
+                i += 1;
+                times += 1;
+            } else {
+                SESE_DEBUG("empty hit");
+            }
+            mutex2.unlock();
+        }
+        auto time = watch.stop();
+        SESE_INFO("time cost: %zu ms", static_cast<size_t>(time.getTotalMilliseconds()));
+    };
+    sese::Thread thread3(proc2, "consumer1");
+    sese::Thread thread4(proc2, "consumer2");
+
+    thread1.start();
+    thread2.start();
+    thread3.start();
+    thread4.start();
+    thread1.join();
+    thread2.join();
+    thread3.join();
+    thread4.join();
+    ASSERT_EQ(times, total);
+}
---
+++ sese/container/RingQueue.h
@@ -0,0 +1,69 @@
+/// \file RingQueue.h
+/// \brief 环形队列
+/// \author kaoru
+/// \date 2024/02/17
+/// \version 1.0
+
+#pragma once
+
+#include <stdexcept>
+
+namespace sese {
+
+/**
+ * 环形队列
+ * @tparam T 元素类型
+ * @tparam N 队列大小
+ */
+template<typename T, int N>
+class RingQueue {
+public:
+    // 获取队头元素，元素不足则抛出异常
+    T &front() {
+        if (empty()) {
+            throw std::out_of_range("queue is empty");
+        }
+        return _data[_head];
+    }
+
+    // 出队，元素不足则抛出异常
+    void pop() {
+        if (empty()) {
+            throw std::out_of_range("queue is empty");
+        }
+        _head = (_head + 1) % N;
+        _size -= 1;
+    }
+
+    // 入队，队列已满则抛出异常
+    void push(const T &data) {
+        if (full()) {
+            throw std::out_of_range("queue is full");
+        }
+        _data[_tail] = data;
+        _tail = (_tail + 1) % N;
+        _size += 1;
+    }
+
+    // 判断队列是否为空
+    [[nodiscard]] bool empty() const {
+        return _size == 0;
+    }
+
+    // 判断队列是否已满
+    [[nodiscard]] bool full() const {
+        return _size == N;
+    }
+
+    // 获取队列大小
+    [[nodiscard]] size_t size() const {
+        return _size;
+    }
+
+private:
+    T _data[N];
+    size_t _head{};
+    size_t _tail{};
+    size_t _size{};
+};
+} // namespace sese
