4edee84789bb6568f03ad7f49d797345d4f0a7c8
kaoru <shiina_kaoru@outlook.com>
2024-02-23 16:25:45 +0800

    feat: http client with proxy
    Close #38

---
+++ sese/internal/net/AsioIPConvert.h
@@ -0,0 +1,30 @@
+#include "sese/net/IPv6Address.h"
+#include <asio.hpp>
+
+asio::ip::address convert(const sese::net::IPAddress::Ptr &addr) {
+    if (addr->getRawAddress()->sa_family == AF_INET) {
+        auto ipv4 = (sockaddr_in *) addr->getRawAddress();
+        return asio::ip::make_address_v4(htonl(ipv4->sin_addr.s_addr));
+    } else {
+        auto ipv6 = (sockaddr_in6 *) addr->getRawAddress();
+        std::array<unsigned char, 16> bytes = {
+                ipv6->sin6_addr.s6_addr[0],
+                ipv6->sin6_addr.s6_addr[1],
+                ipv6->sin6_addr.s6_addr[2],
+                ipv6->sin6_addr.s6_addr[3],
+                ipv6->sin6_addr.s6_addr[4],
+                ipv6->sin6_addr.s6_addr[5],
+                ipv6->sin6_addr.s6_addr[6],
+                ipv6->sin6_addr.s6_addr[7],
+                ipv6->sin6_addr.s6_addr[8],
+                ipv6->sin6_addr.s6_addr[9],
+                ipv6->sin6_addr.s6_addr[10],
+                ipv6->sin6_addr.s6_addr[11],
+                ipv6->sin6_addr.s6_addr[12],
+                ipv6->sin6_addr.s6_addr[13],
+                ipv6->sin6_addr.s6_addr[14],
+                ipv6->sin6_addr.s6_addr[15]
+        };
+        return asio::ip::make_address_v6(bytes, ipv6->sin6_scope_id);
+    }
+}
