cc9add5296191425466f4f46cd52023398761e49
kaoru <shiina_kaoru@outlook.com>
2024-04-09 00:22:08 +0800

    ci: remove old impl

--- ci/win_build.py
+++
@@ -1,4 +0,0 @@
-import ci.common.build as build
-from ci.common.build import BuildType
-
-build.build_ninja_config_all_with('build', BuildType.RELEASE)
--- ci/win_check.py
+++
@@ -1,5 +0,0 @@
-import ci.common.check as ch
-
-assert ch.check_vcpkg()
-assert ch.check_cmake()
-assert ch.check_ctest()
