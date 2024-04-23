79d9283e77c390a55004d9e962c70e7f4c0877d3
kaoru <shiina_kaoru@outlook.com>
2024-04-23 17:23:01 +0800

    refactor: v3::HttpConnection

--- sese/service/http/HttpService_V3.cpp
+++ sese/service/http/HttpService_V3.cpp
@@ -1,17 +1,116 @@
 #include <sese/service/http/HttpService_V3.h>
+#include <sese/internal/net/AsioIPConvert.h>
+#include <sese/internal/net/AsioSSLContextConvert.h>
+#include <sese/net/http/HttpUtil.h>
+#include <sese/io/FakeStream.h>
+#include <sese/Util.h>
 
 using namespace sese::service::http::v3;
 
+inline auto toNumber(const std::string &str) {
+    char *end;
+    return static_cast<size_t>(std::strtol(str.c_str(), &end, 10));
+}
+
+HttpService::HttpService()
+    : io_context(),
+      acceptor(io_context) {
+}
+
 bool HttpService::startup() {
-    return false;
+    auto addr = internal::net::convert(address);
+    auto endpoint = asio::ip::tcp::endpoint(addr, address->getPort());
+    error = acceptor.bind(endpoint, error);
+    if (error) return false;
+
+    error = acceptor.set_option(asio::socket_base::reuse_address(true), error);
+    if (error) return false;
+
+    error = acceptor.bind(endpoint, error);
+    if (error) return false;
+
+    error = acceptor.listen(asio::socket_base::max_listen_connections, error);
+    if (error) return false;
+    return true;
 }
 
 bool HttpService::shutdown() {
-    return false;
+    error = acceptor.close(error);
+    return !error;
 }
 
 int HttpService::getLastError() {
-    return 0;
+    return error.value();
 }
 
+void HttpConnection::readNextLine(const HttpConnection::Ptr &conn) {
+    asio::async_read_until(conn->socket, conn->buffer_reader, "\r\n", [conn](const asio::error_code &error, std::size_t size) {
+        if (size == 0) {
+            auto std_input = std::istream{&conn->buffer_reader};
+            auto stream = io::StdInputStreamWrapper(std_input);
+            auto parse_status = net::http::HttpUtil::recvRequest(&stream, &conn->request);
+            conn->buffer_reader.consume(conn->buffer_reader.size());
+            conn->expect_length = toNumber(conn->request.get("content-length", "0"));
+            conn->real_length = 0;
+            readBody(conn);
+        } else {
+            readNextLine(conn);
+        }
+    });
+}
 
+void HttpConnection::readBody(const HttpConnection::Ptr &conn) {
+    asio::async_read(conn->socket, conn->buffer_reader, [conn](asio::error_code &code, std::size_t bytes_transferred) {
+        conn->real_length += bytes_transferred;
+        if (conn->expect_length == conn->real_length) {
+            handleRequest(conn);
+        } else {
+            readBody(conn);
+        }
+    });
+}
+
+void HttpConnection::handleRequest(const HttpConnection::Ptr &conn) {
+    conn->service->handleRequest(conn);
+    conn->response.set("content-length", std::to_string(conn->response.getBody().getLength()));
+    auto std_output = std::ostream{&conn->buffer_writer};
+    auto stream = io::StdOutputStreamWrapper(std_output);
+    net::http::HttpUtil::sendResponse(&stream, &conn->response);
+
+    conn->real_length = 0;
+    conn->expect_length = conn->buffer_writer.size();
+    writeHeader(conn);
+}
+
+void HttpConnection::writeHeader(const HttpConnection::Ptr &conn) {
+    asio::async_write(conn->socket, conn->buffer_writer, [conn](asio::error_code &code, std::size_t bytes_transferred) {
+        conn->real_length += bytes_transferred;
+        if (conn->expect_length == conn->real_length) {
+            conn->buffer_writer.consume(conn->buffer_writer.size());
+            conn->expect_length = conn->response.getBody().getLength();
+            conn->real_length = 0;
+
+            auto std_output = std::ostream{&conn->buffer_writer};
+            auto stream = io::StdOutputStreamWrapper(std_output);
+            streamMove(&stream, &conn->response.getBody(), conn->expect_length);
+
+            writeBody(conn);
+        } else {
+            writeHeader(conn);
+        }
+    });
+}
+
+void HttpConnection::writeBody(const HttpConnection::Ptr &conn) {
+    asio::async_write(conn->socket, conn->buffer_writer, [conn](asio::error_code &code, std::size_t bytes_transferred) {
+        conn->real_length += bytes_transferred;
+        if (conn->expect_length == conn->real_length) {
+            conn->buffer_writer.consume(conn->buffer_writer.size());
+            conn->expect_length = 0;
+            conn->real_length = 0;
+            // done
+        } else {
+            writeBody(conn);
+        }
+    });
+}
--- sese/service/http/HttpService_V3.h
+++ sese/service/http/HttpService_V3.h
@@ -1,15 +1,44 @@
 #pragma once
 
 #include <sese/service/Service.h>
-#include <sese/service/http/HttpConnection_V3.h>
 #include <sese/net/http/ControllerGroup.h>
 #include <sese/net/IPv6Address.h>
 #include <sese/security/SSLContext.h>
 
+#include <asio.hpp>
+
 namespace sese::service::http::v3 {
 
-class HttpService final : public Service {
+struct HttpConnection;
+class HttpService;
+
+struct HttpConnection {
+    using Ptr = std::shared_ptr<HttpConnection>;
+
+    net::http::Request request;
+    net::http::Response response;
+
+    asio::ip::tcp::socket socket;
+    // void *ssl;
+    asio::streambuf buffer_reader;
+    asio::streambuf buffer_writer;
+
+    size_t expect_length;
+    size_t real_length;
+
+    std::shared_ptr<HttpService> service;
+
+    static void readNextLine(const HttpConnection::Ptr& conn);
+    static void readBody(const HttpConnection::Ptr& conn);
+    static void handleRequest(const HttpConnection::Ptr &conn);
+    static void writeHeader(const HttpConnection::Ptr &conn);
+    static void writeBody(const HttpConnection::Ptr &conn);
+};
+
+class HttpService : public Service, public std::enable_shared_from_this<HttpService> {
 public:
+    HttpService();
+
     bool startup() override;
     bool shutdown() override;
     int getLastError() override;
@@ -39,6 +68,7 @@ public:
         return std::move(*this);
     }
 
+    void handleRequest(const HttpConnection::Ptr &conn);
 private:
     net::IPAddress::Ptr address;
     security::SSLContext::Ptr ssl_context;
@@ -46,6 +76,12 @@ private:
     std::string serv_name;
     std::map<std::string, std::string> mount_points;
     std::map<std::string, net::http::Controller> controllers;
+
+    asio::io_context io_context;
+    asio::ip::tcp::acceptor acceptor;
+    asio::error_code error;
+
+    void startAccept();
 };
 
 } // namespace sese::service::http::v3
