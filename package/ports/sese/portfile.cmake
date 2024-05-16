6ae9f6c0092a605b45e1224da32ad78fbf1ebbda
kaoru <shiinasama2001@gmail.com>
2024-05-16 15:23:59 +0800

    test: add package project

---
+++ package/ports/sese/portfile.cmake
@@ -0,0 +1,48 @@
+function(remove_empty_directories_recursive dir)
+    file(GLOB before_subdirs "${dir}/*")
+    foreach (subdir ${before_subdirs})
+        if (IS_DIRECTORY ${subdir})
+            remove_empty_directories_recursive(${subdir})
+        endif ()
+    endforeach ()
+    file(GLOB after_subdirs "${dir}/*")
+    if ("${after_subdirs}" STREQUAL "")
+        file(REMOVE_RECURSE "${dir}")
+    endif ()
+endfunction()
+
+set(SOURCE_PATH ${CURRENT_BUILDTRESS_DIR}/sese)
+
+vcpkg_from_github(
+        OUT_SOURCE_PATH SOURCE_PATH
+        REPO libsese/sese
+        REF 91c3dd4c2dbb3dbf605f8522f9abbc7a68bcb76b
+        SHA512 2be92338c67d52be493ece4fe044337e02bffc49256fd43051cad020f09e22e548305eb3a812962f8cb6a9ed7964749e23f6cf572c7cdc8b65eb4f366d9790cc
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+        FEATURES
+        tests        SESE_BUILD_TEST
+        mysql        SESE_DB_USE_MARIADB
+        sqlite3      SESE_DB_USE_SQLITE
+        psql         SESE_DB_USE_POSTGRES
+        async-logger SESE_USE_ASYNC_LOGGER
+        archive      SESE_USE_ARCHIVE
+)
+
+vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS ${FEATURE_OPTIONS}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/cmake/sese")
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+remove_empty_directories_recursive("${CURRENT_PACKAGES_DIR}/include/sese")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE" "${SOURCE_PATH}/NOTICE")
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
