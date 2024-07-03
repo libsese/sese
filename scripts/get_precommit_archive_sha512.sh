d8f7874ae72deee214838b6db55d3e9a5aed5637
kaoru <shiinasama2001@gmail.com>
2024-07-03 21:05:03 +0800

    build: dependency packaging validation

--- scripts/get_precommit_archive_sha512.sh
+++ scripts/get_precommit_archive_sha512.sh
@@ -3,6 +3,6 @@
 hash_code=$( git -C . --no-pager log -1 --pretty=format:%H)
 file_name="$hash_code".tar.gz
 wget https://github.com/libsese/sese/archive/"$file_name"
-archive_hash_code=$(vcpkg hash "$file_name")
+archive_hash_code=$(sha512sum $file_name | awk '{ print $1 }')
 echo "commit hash $hash_code"
 echo "archive hash $archive_hash_code"
