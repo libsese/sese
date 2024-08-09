10712af73f482a6b0890c8feb337e5e031a66384
kaoru <shiina_kaoru@outlook.com>
2024-08-09 11:41:00 +0800

    import sese-core as submodule

---
+++ src/test/Main.cpp
@@ -0,0 +1,12 @@
+#include <sese/Init.h>
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    sese::initCore(argc, argv);
+#if defined(__linux__) || defined(__APPLE__)
+    signal(SIGPIPE, SIG_IGN);
+#endif
+    return RUN_ALL_TESTS();
+}
