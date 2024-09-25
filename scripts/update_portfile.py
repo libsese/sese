a714816ccf6342f2023401ea5765f9f54f7629f9
kaoru <shiina_kaoru@outlook.com>
2024-09-25 11:55:24 +0800

    ci: .

--- scripts/update_portfile.py
+++ scripts/update_portfile.py
@@ -1,6 +1,9 @@
+import os
 import subprocess
 import requests
 import hashlib
+
+from pygments.lexers.robotframework import Variable
 
 # 获取最新的 commit hash
 result = subprocess.run(['git', '-C', '.', '--no-pager', 'log', '-1', '--pretty=format:%H'], stdout=subprocess.PIPE)
@@ -20,3 +23,20 @@ archive_hash_code = sha512.hexdigest()
 # 输出结果
 print(f"commit hash {hash_code}")
 print(f"archive hash {archive_hash_code}")
+
+variables = {
+    'REF': hash_code,
+    'SHA512': archive_hash_code
+}
+
+current_dir = os.path.dirname(__file__)
+with open(os.path.join(current_dir, '../package/ports/sese/portfile.cmake.in'), 'r') as infile:
+    template = infile.read()
+
+for key, value in variables.items():
+    template = template.replace(f'@@{key}@@', value)
+
+with open(os.path.join(current_dir, '../package/ports/sese/portfile.cmake'), 'w') as outfile:
+    outfile.write(template)
+
+print('ok')
