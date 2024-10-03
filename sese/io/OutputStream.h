9d8764fa13b9d0259069ea60b328f0a8775eb9c2
kaoru <shiina_kaoru@outlook.com>
2024-10-03 17:03:52 +0800

    feat: Add support for writing std::vector, std::array, and std::span to OutputStream
    - Extended OutputStream to accept std::vector, std::array, and std::span types.
    - Included warning note on considering POD type requirements for non-POD types.
    - Enhanced OutputStream interface for broader input compatibility.

--- sese/io/OutputStream.h
+++ sese/io/OutputStream.h
@@ -8,12 +8,16 @@
 #pragma once
 
 #include "sese/Config.h"
+#include "sese/text/String.h"
+
+#include <span>
 
 namespace sese::io {
 
 // GCOVR_EXCL_START
 
 /// \brief 流式输出接口类
+/// \warning 支持写入 std::vector、std::array、std::span 等类型，但需要注意 T 是否为 POD 类型，非 POD 类型写入将会导致不可预期事件
 class  OutputStream {
 public:
     typedef std::shared_ptr<OutputStream> Ptr;
@@ -21,6 +25,25 @@ public:
     virtual ~OutputStream() noexcept = default;
 
     virtual int64_t write(const void *buffer, size_t length) = 0;
+
+    int64_t write(const std::string_view &buffer);
+
+    int64_t write(const text::StringView &buffer);
+
+    template<typename T>
+    int64_t write(std::vector<T> &buffer) {
+        return write(buffer.data(), buffer.size());
+    }
+
+    template<typename T, size_t N>
+    int64_t write(std::array<T, N> &buffer) {
+        return write(buffer.data(), N * sizeof(T));
+    }
+
+    template<typename T>
+    int64_t write(std::span<T> &buffer) {
+        return write(buffer.data(), buffer.size() * sizeof(T));
+    }
 };
 
 // GCOVR_EXCL_STOP
